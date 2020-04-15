0163e49ea518967225755d2292f0aa5f2b8f3144
Cory Lynch <cjl2625@gmail.com>
2020-04-14 23:48:38 -0400

    Update auth config to work in production

--- server/auth.js
+++ server/auth.js
@@ -6,14 +6,15 @@ const OAuth2Strategy = require("passport-oauth2").Strategy;
 
 const router = express.Router();
 const User = require("./models/user");
+const prod = process.env.NODE_ENV === "production";
 
 passport.use(
   new OAuth2Strategy(
     {
       authorizationURL: "https://osu.ppy.sh/oauth/authorize",
       tokenURL: "https://osu.ppy.sh/oauth/token",
-      clientID: "915",
-      clientSecret: "L2biRnz0bu4IxaL0ircnF42yhyKwCZGYcezo7GiQ",
+      clientID: prod ? process.env.PROD_CLIENT_ID : process.env.DEV_CLIENT_ID,
+      clientSecret: prod ? process.env.PROD_CLIENT_SECRET : process.env.DEV_CLIENT_SECRET,
       callbackURL: "/auth/osu/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
