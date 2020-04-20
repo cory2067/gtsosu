e1d6be77ca9cb4470a1c5a20a922f42c3a5cfb3b
Cory Lynch <cjl2625@gmail.com>
2020-04-19 23:30:09 -0400

    Add buttons on tourney home pages

--- client/src/components/pages/TourneyHome.js
+++ client/src/components/pages/TourneyHome.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "../../utilities.css";
 import "./TourneyHome.css";
 
-import { Layout, Card } from "antd";
+import { Layout, Card, Button } from "antd";
 const { Content } = Layout;
 
 class TourneyHome extends Component {
@@ -16,8 +16,24 @@ class TourneyHome extends Component {
   render() {
     return (
       <Content className="content">
-        <h1>this is {this.props.tourney}</h1>
-        todo put the info stuff, links, description here
+        <h1>{this.props.tourney.toUpperCase()}</h1>
+        <div className="u-flex-justifyCenter">
+          <div className="TourneyHome-info">
+            <div className="TourneyHome-description">
+              The Expert Global Taiko Showdown, which is our 1v1 tournament targeted towards top
+              players, even though it has no rank limit. The top 128 of it after qualifiers will
+              face-off in a heated double-elimination bracket.
+            </div>
+            <div className="TourneyHome-button-box">
+              <Button type="primary" size="large">
+                Register
+              </Button>
+              <Button type="primary" size="large" href="http://google.com">
+                Discord
+              </Button>
+            </div>
+          </div>
+        </div>
         <div className="TourneyHome-cardbox">
           <Card title="Restrictions" bordered={true}>
             u gotta be GOOD to play this tourny
