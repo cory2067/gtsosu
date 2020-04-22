54bd319139337effa7399d8920e505a49e826129
Kasumiikun <Kasumiikun@users.noreply.github.com>
2020-04-22 04:30:30 +0200

    Update egts.js

--- client/src/content/egts.js
+++ client/src/content/egts.js
@@ -48,7 +48,19 @@ July 25th until July 26th.`,
 - This tournament is a **1v1 double-elimination tournament.** 
 - All players will first play in the qualifiers stage, then the top 128 players will be playing in a **double-elimination bracket.**
 - The scoring system that will be used is **Score V2.**
+- Any members of staff apart from the **Hosts**, **Mappool Selectors**, and **Referees** are allowed to play.
+- Players and staff members must have read this ruleset entirely. (and we will assume you did so throughout the tournament)
+- All participants must stay respectful and keep a proper attitude. Not following this rule can result in a ban/blacklist from the tournament. This rule concerns all the staff as well.
+- Any rule changes or unexpected occurrences will be announced in the thread and in the Discord server.
 
+# Stage Regulations
+- There are nine stages in the 1v1 tournament, the Qualifiers, the RO128, the RO64, the RO32, the RO16, the Quarterfinals, the Semifinals, the Finals, and the Grand Finals. It may change depending on if a region gets less players than expected, or much more than expected.
+- Players will be seeded with the average of their ranks on each map during the Qualifiers round.
+- In case there are more than 128 registered players, only the top 128 of the Qualifiers will go in the rest of the tournament.
+- The winning condition for each stage are listed below:
+    - Round Of 32, Round Of 16: Best-Of-9 (First to 5 points)
+    - Quarterfinals: Best-Of-11 (First to 6 points)
+    - Semifinals, Finals, and Grand Finals: Best-Of-13 (First to 7 points)
 # Mappool Instructions
 - There will be 1 mappool for each of the following: Group Stage, Round of 16, Quarterfinals, Semifinals, Finals, and Grand Finals. A different pool will be prepared for All-star (see All-star Rules for more details).
 - The Loser's Bracket will play on the same pool as the Winner's bracket of the same weekend.
