ed24cd23e99c93632da9bd3ed79f3713cf5ab83a
Kasumiikun <Kasumiikun@users.noreply.github.com>
2020-04-22 06:42:11 +0200

    Update EGTS page with full rules

--- client/src/content/egts.js
+++ client/src/content/egts.js
@@ -58,12 +58,49 @@ July 25th until July 26th.`,
 - Players will be seeded with the average of their ranks on each map during the Qualifiers round.
 - In case there are more than 128 registered players, only the top 128 of the Qualifiers will go in the rest of the tournament.
 - The winning condition for each stage are listed below:
-    - Round Of 32, Round Of 16: Best-Of-9 (First to 5 points)
+    - Round Of 128, Round Of 64, Round Of 32, Round Of 16: Best-Of-9 (First to 5 points)
     - Quarterfinals: Best-Of-11 (First to 6 points)
     - Semifinals, Finals, and Grand Finals: Best-Of-13 (First to 7 points)
+
 # Mappool Instructions
-- There will be 1 mappool for each of the following: Group Stage, Round of 16, Quarterfinals, Semifinals, Finals, and Grand Finals. A different pool will be prepared for All-star (see All-star Rules for more details).
+- There will be 1 mappool for each of the following: Qualifiers, Round of 128, Round of 64, Round of 32, Round of 16, Quarterfinals, Semifinals, Finals, and Grand Finals. 
 - The Loser's Bracket will play on the same pool as the Winner's bracket of the same weekend.
+- The Qualifiers pool will be different from all the other rounds, as it will have a format of 2 NoMod maps, 1 Hidden map, 1 HardRock map, 1 DoubleTime map.
 - Each mappool consists of 6 brackets: **NoMod, Hidden, HardRock, DoubleTime, FreeMod, and Tiebreaker**.
+- The mappools from Round of 128 until Round of 16 will have 14 maps each. The quarterfinals mappool will have 15 maps. The mappools from Semifinals until Grand Finals will have 19 maps.
+- The size of the NoMod bracket will be of 4 in Round Of 128 until Round Of 16, it will be of 5 in Quarterfinals, and it will be of 6 in Semifinals until Grand Finals.
+- The size of the Hidden, Hard Rock, and Double Time will be of 2 from RO128 until Quarterfinals, and it will be of 3 from Semifinals until Grand Finals.
+- The size of the FreeMod pool will be of 3 during the rest of the tournament.
+- The FreeMod pool is played with FreeMod activated. Every individual player can pick Hidden, HardRock or both. **At least one player of each team must have at least one mod activated.**
+- The Tiebreaker is played under FreeMod conditions but it is not necessary that at least one player has at least one mod activated.
+- The mappool for each stage will be showcased a week before.
+
+# Scheduling Instructions
+- Every stage will be held **during the weekend**. (it is however allowed to schedule on another day if no good time is found during the weekend and a referee is available)
+- We will try to schedule as best as possible to fit with the timezones. The schedule will be released the previous weekend of each stage.
+- Reschedules will only be considered if both players agree to a time, this needs to be done and notified to the tournament staff before **Thursday at 23:59 UTC** in that particular week when your match takes place.
+
+# Match Regulations
+- A private lobby will be created 10 minutes before your match by a referee. Both players will be invited into the room when it's match time. 
+- If no staff or referee is available the match will be postponed.
+- If one of the players is late, the maximum time the match can be postponed is 10 minutes.
+- There is no penalty for not joining the room instantly after match time as long as it's within the postponed time.
+- If neither of the players are in the lobby after 10 minutes, the highest seeded player will advance.
+- The match will be played as Head to Head and ScoreV2 and the name of the room must be: "EGTS 2020: (Player Red) vs (Player Blue)".
+- Each player is allowed one warm-up but it must be below 4 minutes. 
+- If a player takes more than 2 mins to decide on a warm-up the referee will be skipping It.
+- Each player has to ban **one beatmap** to be selected from the pool (apart from Tiebreaker). These beatmaps are not allowed to be picked by any player in the entire match.
+- Each player will roll with the command "!roll" in the chat.
+- The player with the **highest roll bans second and picks first**. The player with **the lowest roll bans first and picks second**.
+- Each player has 2 mins to announce their ban. If they didn't tell the referee by that time the ban will be made with a roll.
+- Beatmap selection will then alternate between each player selecting a beatmap out of the map pool.
+- Players may pick freely from any bracket.
+- Players will have 2 minutes to pick a beatmap and 2 minutes to get ready. If a player didn't tell their pick before the end of the timer the map will be picked with a roll. And if players do not get ready before the end of the roll, the match will be started, no matter what.
+- If a player disconnects between the beatmaps, the match can be delayed 10 minutes at maximum. This delay can only be applied once.
+- If a player disconnects after 30 seconds, they get treated as if they lost the map.
+- Disconnects within 30 seconds after beatmap begin can be rematched. This is only allowed once per player.
+- Failed players' scores do count.
+- If the beatmap ends in a draw the game will be nullified and the map will be replayed. If it does end in a tie again, the map will be nullified again, and the player who picked it will have to pick another map.
+- In case of a tie in points (example: 4-4 in BO9) the tiebreaker is selected.
   `,
 };
