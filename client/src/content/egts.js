6e2ffc446e5062725b5479c484ffb053b3f39524
Kasumiikun <Kasumiikun@users.noreply.github.com>
2020-04-22 03:18:06 +0200

    Update egts.js

--- client/src/content/egts.js
+++ client/src/content/egts.js
@@ -6,31 +6,48 @@ export default {
   homepage: [
     {
       title: "Restrictions",
-      body: `there is **no** rank limit`,
+      body: `There are no rank restrictions or country restrictions. Any osu!taiko player who has not been restricted within the past 12 months will be allowed in this tournament. The osu! support reserves the right from removing any player from the tournament following the screening phase.`,
     },
     {
       title: "Dates",
       body: `
+### Registration Phase
+May 3rd until May 17th.
+### Screening phase
+May 17th until May 24th.
+### Qualifiers
+May 30th until May 31st.
+### Round of 128
+June 6th until June 7th.
+### Round of 64
+June 13th until June 14th.
+### Round of 32
+June 20th until June 21st.
+### Round of 16
+June 27th until June 28th.
 ### Quarterfinals
-tomorrow
+July 4th until July 5th.
 ### Semifinals
-the next day
+July 11th until July 12th.
 ### Finals
-*never*`,
+July 18th until July 19th.
+### Grand Finals
+July 25th until July 26th.`,
     },
-    { title: "Registration", body: "just clck the register button lmao" },
+    { title: "Registration", body: "Please click on the Register button above after you've logged in with your osu! account, then click on the Discord button to join the server of our tournament. **Joining the Discord is mandatory to play in this tournament, and you will be removed from the tournament if you don't do so.** }, 
     {
       title: "Prizes",
       body: `
-- **1st place:** $5
-- **2nd place:** $4`,
+- **1st place:** $26 or one year of supporter, and a GTS-themed mousepad made by [Foxbox](https://foxbox.io)
+- **2nd place:** $16 or six months of supporter, and a GTS-themed mousepad made by [Foxbox](https://foxbox.io)
+- **3rd place:** $8 or two months of supporter, and a GTS-themed mousepad made by [Foxbox](https://foxbox.io)`,
     },
   ],
   rules: `
 # General Rules
-sklgdf
-dfsklg;dfkg
-sdfk;lgkfdl;gkdf;
+- This tournament is a **1v1 double-elimination tournament.** 
+- All players will first play in the qualifiers stage, then the top 128 players will be playing in a **double-elimination bracket.**
+- The scoring system that will be used is **Score V2.**
 
 # Mappool Instructions
 - There will be 1 mappool for each of the following: Group Stage, Round of 16, Quarterfinals, Semifinals, Finals, and Grand Finals. A different pool will be prepared for All-star (see All-star Rules for more details).
