ebc245940f1a4903f9c24579693230f28f42317f
Briesmas <mrbriesmas@gmail.com>
2020-05-12 05:17:40 +0200

    Add UI and IGTS French translation

---
+++ client/src/content/igts-fr.js
@@ -0,0 +1,160 @@
+export default {
+  name: "Intermediate Global Taiko Showdown",
+  description:
+    "L'Intermediate Global Taiko Showdown, le tournoi fondateur de cette série de tournois. C'est notre tournoi 2vs2 pour les joueurs de niveau intermédaire limité pour les rangs #3500 à #100000. Les équipes vont d'abord jouer une qualification. Les 32 meilleures vont ensuite jouer dans une phase de groupe d'où 16 équipes vont sortir pour jouer dans un bracket à double élimination.",
+  discord: "https://discord.gg/a6PzzFz",
+  homepage: [
+    {
+      title: "Restrictions",
+      body: `1. Vous devez être entre les rangs #3500 et #10000 pendant la phase d'inscription. Cela signifie que vous devez être entre ces rangs jusqu'à la fin de la période d'inscription. 
+2. Vous n'avez pas le droit de monter plus haut que le rang #2500 pendant la durée du tournoi. Si vous dépassez cette limite vous serez disqualifié. 
+3. Vous devez rejoindre le serveur Discord du tournoi.
+4. N'importe quel joueur osu!taiko n'ayant pas été restrict dans les 12 derniers mois pourra jouer ce tournoi. Le support osu! se réserve le droit de retirer un joueur de la liste des participants au tournoi après la phase de Screening.`,
+    },
+    { 
+      title: "Inscription", 
+     body: `**Merci de cliquer sur le bouton S'inscrire juste au-dessus après que vous vous soyez connecté avec votre compte osu! et ensuite cliquez sur le bouton Discord pour rejoindre le serveur de ce tournoi.**
+
+**Rejoindre le serveur Discord est obligatoire pour jouer dans ce tournoi et vous serez refusé du tournoi si vous ne le faites pas.**`,
+    }, 
+    {
+     title: "Dates",
+      body: `
+### Phase d'inscription
+2 Mai 23:59 UTC - 23 Mai 23:59 UTC 
+### Screening et création des équipes 
+23 Mai - 30 Mai 
+### Qualifications  
+6 Juin - 7 Juin 
+Phase de groupe
+13 Juin - 14 Juin 
+### Huitièmes de finale
+20 Juin - 21 Juin 
+### Quarts de finale et Loser's Bracket Round 1 et 2
+27 Juin - 28 Juin 
+### Demi-finales + Loser's Bracket Round 3 et 4
+4 Juillet - 5 Juillet 
+### Finale du Winner's Bracket + Finale et Grande Finale du Loser's Bracket
+11 Juillet - 12 Juillet 
+### All-Star + Grande Finale
+18 Juillet - 19 Juillet `,
+    },
+    {
+      title: "Prix",
+      body: `
+- **1ère place:** badge de profile (à confirmer), $64 ou 6 mois de supporter par joueur et une bannière d'équipe pour la première place 
+- **2ème place:** $48 ou 4 mois de supporter par joueur et une bannière d'équipe pour la deuxième place 
+- **3ème place:** $32 ou 2 mois de supporter par joueur et une bannière d'équipe pour la troisième place `,
+    },
+],
+  rules: `
+# Règles générales
+ - Ce tournoi est un tournoi **par équipe 2v2** avec des équipes de **5 joueurs maximum**.
+ - Les équipes vont d'abord passer par des qualifications. Elles vont ensuite jouer dans une **phase de groupe** et enfin dans un **bracket à double élimination**.
+ - Le système de score utilisé sera le Score V2. 
+ - N'importe quel joueur satisfaisant les limites indiquées dans la partie "Limitations sur le rang" est autorisé à jouer. Puisque le tournoi désire être badgé, certains joueurs peuvent être refusés par le staff d'osu.
+ - Tout membre du staff mis à part les **Organisateurs, mappoolers et les arbitres** sont autorisés à jouer.
+ - Les joueurs et le staff doivent avoir lu l'ensemble des règles (et nous supposerons que vous l'avez fait tout au long du tournoi).
+ - Tous les participants doivent rester respectueux et garder un comportement correct. Si vous dérogez à cette règle vous pouvez être ban/blacklist de ce tournoi. Cette règle concerne aussi le staff.
+ - Tout changement dans les règles ou toutes circonstances non prévues seront signalés dans le serveur Discord.
+
+# Inscription au tournoi et formation des équipes
+ - Pour pouvoir participer à ce tournoi, vous devez rentrer dans les limitations imposées dans la partie **limitations sur le rang**.
+ - Chaque joueur s'inscrit individuellement. Lorsque leur inscription a été acceptée, le joueur obtiendra un rôle correspondant à son pays.
+ - Le staff se permet de refuser un joueur s'il estime que son niveau n'est pas en adéquation avec celui du tournoi. 
+ - Cette année nous autorisons 2 équipes pour un pays sous certaines conditions :
+   - Il y a **7 joueurs ou plus ** inscrits pour ce pays.
+   - Chaque équipe doit avoir **au moins 3 joueurs**.
+ - Par conséquence à la règle précédente, plusieurs situations vont se présenter pour former les équipes :
+   - S'il y a 5 joueurs ou moins pour un pays, ces joueurs formeront l'équipe qui représentera ce pays.
+   - S'il y a plus de 10 joueurs inscrits pour un pays, les joueurs décident entre eux ceux qui vont participer au tournoi. Si les joueurs n'arrivent pas à tomber d'accord, les 10 joueurs avec le meilleur rang seront choisis pour former les 2 équipes.
+   - S'il y a 6 joueurs, les joueurs décident quels seront les 5 qui participeront dans le tournoi. Si les joueurs n'arrivent pas à tomber d'accord, les 5 joueurs avec le meilleur rang seront choisis pour former l'équipe.
+   - S'il y a 7 à 10 joueurs (après avoir gardé les meilleurs) inscrits pour un pays, les joueurs de ce pays auront quelques jours pour décider sur la manière de partager l'effectif dans les 2 équipes.
+  Il est conseillé de trouver un accord rapidement pour ensuite pouvoir choisir le capitaine de l'équipe et ainsi pouvoir plannifier et préparer vos matches rapidement. 
+   - Dans le cas où les joueurs n'arrivent pas à se mettre d'accord, ils seront classés par pp et mis dans 2 équipes avec chacune au moins 3 membres avec les meilleurs classés dans une première équipe et les autres dans la deuxième.
+   - Les joueurs choisissent le capitaine de leur équipe.
+   - Si les joueurs n'arrivent pas à élire un capitaine, le joueur avec le plus haut rang sera choisit en tant que capitaine.
+ - Le capitaine est celui qui va discuter avec l'équipe adverse pour modifier l'horaire d'un match ou encore s'assurer que ses coéquipiers soient présent pour le match.
+
+# Déroulement du tournoi
+ - Il y a 7 tours dans ce tournoi : les Qualifications, la phase de groupe, les Huitièmes de finale, les Quarts de finale, les Demi-finales, les Finales et les Grandes Finales.
+ - Les équipes seront seed en fonction de la moyenne de leur classement sur chaque map pendant les qualifications dans 4 seeds : Top seed, High seed, Low seed et Unseeded. (Le processus de seeding est plus détaillé dans la partie instructions pour les qualifications)
+ - Les équipes vont être tirées au sort dans **8 groupes de 4 équipes** pendant un drawing show qui prendra place le dimanche 6 juin.
+ - Pendant la phase de groupe toutes les équipes vont jouer une fois contre toutes les autres équipes. Les **2 meilleures équipes** sortent de leur groupe.
+ - Le classement dans un groupe est décidé selon les résultats de chaque équipe dans l'ordre de priorité suivante :
+   - Nombre de matches gagnés le plus élevé.
+   - Le plus haute différence (nombre de beatmaps gagnées - nombre de beatmaps perdues).
+   - Nombre de beatmaps gagnées le plus élevé.
+   - Le gagnant du match entre les équipes à égalité.
+ - Pendant la phase de groupe, une victoire par défaut est considérée comme un victoire 5:0, avec un ratio de différence de score de +1.0.
+ - La condition de victoire pour chaque tour est :
+   - Phase de groupe : BO9 (premier à 5 maps gagnées).
+   - Huitièmes de finale, Quarts de finale et Loser's bracket round 1 et 2 : BO11 (premier à 6 maps gagnées).
+   - Demi-finales, Finales, Grandes Finales et Loser's Bracket round 3 et 4 : BO13 (premier à 7 maps gagnées).
+
+# Instructions pour les qualifications
+ - Pendant ce tour, plusieurs salles seront disponibles à différents horaires.
+ - Une salle privée sera créée et les équipes seront invitées à l'horaire prévu. Faites bien en sorte d'être en ligne à ce moment.
+ - Pas de warm-up.
+ - Seuls deux joueurs joueront par map. Il n'y pas de limite sur le nombre de changements de joueur.
+ - Le classements se fera en fonction de la moyenne du classement de chaque équipe sur chaque map. 
+ - Les maps seront jouées deux fois. Le meilleur score combiné sera pris en compte pour le classement.
+ - Seules les 32 meilleures équipes avanceront en phase de groupe.
+
+# Déroulement d'un match
+ - Une salle sera créée 10 minutes avant l'horaire du match par un arbitre. Chaque capitaine sera invité dans la salle à l'heure indiquée. Ensuite l'arbitre ou les capitaines inviteront les autres joueurs dans la salle.
+ - Si aucun arbitre ou membre du staff est disponible à l'horaire du match alors celui-ci sera reporté.
+ - S'il y a moins que le nombre minimum de joueurs nécessaire pour effectuer un match à l'heure prévue alors le match peut être retardé au maximum de 10 minutes.
+ - Il n'y a pas de pénalité pour ne pas avoir rejoint la salle juste après l'horaire du match tant que cela reste dans les 10 minutes autorisées.
+ - Si les deux équipes n'ont pas assez de joueurs à la fin des 10 minutes, cela comptera comme une défaite pour les deux équipes pendant la phase de groupe. Dans la phase éliminatoire, c'est l'équipe avec le plus haut seed qui sera déclarée victorieuse.
+ - Le match sera joué en TeamVs et ScoreV2 et le nom de la salle doit être : "IGTS 2020: (Team Red) vs (Team Blue)".
+ - Chaque équipe a droit à un warm up. Celui-ci doit durer moins de 3 minutes. Il est autorisé d'inviter tous les joueurs dans la salle pour les warm ups.
+ - Si une équipe met plus de 2 minutes à choisir leur warm-up alors celui-ci ne sera pas joué.
+ - Chaque capitaine doit bannir **une beatmap** prise dans le pool (sauf le Tiebreaker). Ces beatmaps ne pourront plus être pick par aucune équipe pendant tout le match.
+ - Le capitaine de chaque équipe va faire un jet de dés en tapant la commande "!roll" dans le chat.
+ - L'équipe avec le jet le **plus haut bannit en second et pick en premier**. L'équipe avec le jet le **plus bas bannit en premier et picks en second**.
+ - Chaque capitaine a deux minutes pour annoncer leur ban. Si un capitaine ne communique pas son ban dans le temps imparti alors celui-ci sera fait via un roll.
+ - La sélection de la beatmap à jouer va alterner entre chaque capitaine choisissant une map du pool.
+ - Les capitaines peuvent pick librement dans n'importe quelle catégorie.
+ - Les équipes auront 2 minutes pour choisir une map et 2 minutes pour être prêt. Si une équipe n'annonce pas leur pick dans le temps imparti alors celui-ci sera fait via un roll.
+ - Il est autorisé d'échanger les joueurs sans aucune limitation.
+ - Si un joueur déconnecte entre deux beatmaps et qu'une équipe n'a aucun remplaçant alors le match peut être retardé d'au maximum 10 minutes.
+ - Si un joueur déconnecte, il sera considéré comme ayant fail la map.
+ - Si la déconnexion est arrivée dans les 30 premières secondes d'une beatmap alors il est autorisé de recommencer la map. Cela est autorisé seulement une fois par équipe.
+ - Les scores des joueurs qui ont fail ne sont pas ajouté au score total de l'équipe.
+ - Si une beatmap finit sur une égalité alors le map est nullifiée et rejouée.
+ - Dans le cas d'une égalité des points (exemple: 4-4 en BO9), le Tiebreaker est selectionné.
+
+# Format des mappools
+ - Il y aura 1 mappool pour chacun des tours suivants: qualifications, phase de groupe, huitièmes de finale, quarts de finale, demi-finales, finales et grandes finales. Un pool différent sera préparé pour les All-star(voir la partie dédiée pour plus details).
+ - Les qualifications auront un pool différent des autres rounds. Il sera constitué de 2 NoMod maps, 1 Hidden map, 1 Hard Rock map, 1 Double Time map. 
+ - Le Loser's Bracket jouera sur le même pool que celui joué par le Winner's bracket le même weekend.
+ - Chaque mappool est constitué de 6 parties : NoMod, Hidden, HardRock, DoubleTime, FreeMod et Tiebreaker.
+ - Le mappool de la phase de groupe sera constitué de 14 maps, celui des huitièmes et des quarts de finale de 15 maps et enfin celui des demi-finales, finales et grandes finales de 16 maps.
+ - Il y aura 2 maps Hidden, HardRock et DoubleTime et 3 FreeMod pour tous les rounds.
+ - Les maps en Freemod sont jouées avec FreeMod activé. Chaque joueur peut choisir entre **Hidden, HardRock ou les deux**. **Au moins un joueur de chaque équipe doit avoir au moins un mod activé.**
+ - Le Tiebreaker est joué dans les mêmes conditions que Freemod cependant il n'est pas nécessaire qu'au moins un joueur doit avoir un mod activé.
+ - Le mappool pour chaque tour sera showcased une semaine à l'avance.
+
+# Insctructions pour les horaires
+ - Chaque stage se déroulera **pendant le weekend**. (il est cependant accepté de déplacer un match sur un autre jour si aucun horaire pendant le weekend a été trouvé)
+ - Il est autorisé d'avoir plusieurs matchs en même temps tant qu'il y a assez d'arbitres.
+ - Nous ferons de notre mieux pour plannifier les matchs en accord avec les fuseaux horaires. Le planning des matchs sera dévoilé chaque weekend précédent l'étape concernée.
+ - Un changement d'horaire sera uniquement accepté si les deux équipes se sont mis d'accord sur un horaire. Cela doit être fait et le staff doit en être informé avant le **jeudi à 23:59 UTC+0** de la semaine du match.
+ - Les chagements d'horaire ne peuvent être demandés que par les **capitaines d'équipe**.
+
+# Règles pour le All-star
+ - All-Star est un évènement pour le fun se déroulant le même weekend que la Grande Finale!
+Les meilleurs joueurs du tournoi vont jouer entre eux dans un match spécial.
+ - 8 joueurs vont participer à cet évènement. Ils seront décidés par un vote.
+ - Les 8 joueurs avec le plus de votes vont jouer dans l'All-Star. S'il y a plus de 2 joueurs d'un même pays dans les 8 meilleurs, seuls les deux meilleurs participeront dans l'All-Star.
+ - Les 2 joueurs avec le plus de votes seront capitaine d'une équipe. Leurs coéquipiers seront décidés par le classement des 6 joueurs suivants. Le joueur avec le plus de votes formera une équipe avec les joueurs ayant un classement impair et le deuxième joueur avec le plus de vote avec ceux ayant un classement pair.
+ - Le mappool sera fait par d'autres mappoolers.
+ - L'évènement sera un 4 vs 4 avec les règles suivantes :
+   - BO15.
+   - Le mappool sera constitué de 20 FreeMod maps et 1 TB. Toutes les maps peuvent être jouées avec n'importe quel combinaison de mods. Il sera cependant difficile de mettre un même un seul mod sur certaines d'entre elles !
+   - Pas de bans.
+   - La sélection des maps se fait de la même manière qu'un match normal.
+   - Mêmes règles qu'un match classique pour les déconnections.
+`,
+};
