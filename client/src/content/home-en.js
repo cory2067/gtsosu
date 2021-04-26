4f423c635823ade26e795db9125c649f3feb07c4
Kasumiikun <Kasumiikun@users.noreply.github.com>
2021-04-26 12:11:41 +0200

    Remove CGTS from homepage.
    CGTS isn't planned to be recurring anymore, so irrelevant to have it on homepage for now since it ended.

--- client/src/content/home-en.js
+++ client/src/content/home-en.js
@@ -31,21 +31,6 @@ The top 32 teams after qualifiers will then face off in a Group Stage, where 16 
 The Beginners Global Taiko Showdown, our 2v2 tournament for beginner players, being restricted for ranks #8,000 to no bottom rank limit.
 The top 16 teams after qualifiers will then face off in a Group Stage, where 8 teams will go through, and play in a double-elimination bracket.`,
     },
-    {
-      title: "CGTS",
-      code: "cgts",
-      divisions: [
-        { title: "4v4", code: "4v4" },
-        { title: "Asia", code: "asia" },
-        { title: "Europe", code: "eu" },
-        { title: "North America", code: "na" },
-        { title: "Oceania / Southeast Asia", code: "osea" },
-        { title: "South America", code: "sa" },
-      ],
-      description: `
-The Continental Global Taiko Showdown, our 1v1 tournament with no rank limit, for 5 different regions all at once, for which the top 8 of each then goes to a 4v4 tournament double-elimination bracket.
-The top 32 players after qualifiers in the 1v1 tournaments will then face off in a double-elimination bracket.`,
-},
   ],
 };
 
