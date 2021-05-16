4d8379a3ad30b476ade107b7661ed2ad3709a81e
Kasumiikun <Kasumiikun@users.noreply.github.com>
2021-05-16 15:11:07 +0200

    Add year to tournaments

--- client/src/content/home-en.js
+++ client/src/content/home-en.js
@@ -3,14 +3,14 @@ export default {
   description: "Welcome to the website of one of the biggest osu!taiko tournament series!",
   tournies: [
     {
-      title: "EGTS",
+      title: "EGTS 2020",
       code: "egts",
       description: `
 The Expert Global Taiko Showdown, which is our 1v1 tournament targeted towards top players, even though it has no rank limit. 
 The top 64 of it after qualifiers will face-off in a group stage, followed by a heated double-elimination bracket. Next edition will be released on September 5th.`,
     },
     {
-      title: "IGTS",
+      title: "IGTS 2020",
       code: "igts",
       description: `
 The Intermediate Global Taiko Showdown, the founding tournament of this series.
@@ -18,14 +18,14 @@ It's our 2v2 tournament for intermediate level players being restricted for rank
 The teams will first play in a qualifiers. The top 32 teams will then play in a Group Stage where 16 teams will go through and play in a double-elimination bracket.`,
     },
     {
-      title: "AGTS",
+      title: "AGTS 2021",
       code: "agts",
       description: `
 The Advanced Global Taiko Showdown, our 2v2 tournament for advanced-level players, being restricted for ranks #500 to #3,500. 
 The top 32 teams after qualifiers will then face off in a Group Stage, where 16 teams will go through, and play in a double-elimination bracket.`,
     },
     {
-      title: "RGTS",
+      title: "RGTS 2021",
       code: "rgts",
       description: `
 The Rising Global Taiko Showdown, our 2v2 tournament for mid-range players, being restricted for ranks #1,000 to #4,500.
