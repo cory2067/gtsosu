d7ace69b6036e01335c69978bb5ad0380d0e1448
TsukoteAmie <thepeaceluffer@gmail.com>
2021-11-28 00:47:20 +0100

    Add files via upload

---
+++ client/src/content/sgts_2021-en.js
@@ -0,0 +1,126 @@
+export default {
+  name: "Squad Global Taiko Showdown",
+  description:
+    "The Squad Global Taiko Showdown is our 3v3 tournament where players can decide their own teams up to 6 players. The top 32 teams after qualifiers will face-off in a heated double-elimination bracket.",
+  discord: "https://discord.gg/3mGC3HB",
+  homepage: [
+    {
+      title: "Restrictions",
+      body: `There are no rank restrictions or country restrictions. Any osu!taiko player who has not been restricted within the past 12 months will be allowed in this tournament. The osu! support reserves the right from removing any player from the tournament following the screening phase.`,
+    },
+    {
+      title: "Dates",
+      body: `
+### Registration Phase
+November 27th until December 12th.
+### Screening phase
+December 12th until January 2nd.
+### Qualifiers
+January 8th until January 9th.
+### Round of 32
+January 15th until January 16th.
+### Round of 16 + Loser's Bracket Round 1
+January 22nd until January 23rd.
+### Quarterfinals + Loser's Bracket Round 2 and 3
+January 29th until January 30th.
+###  Semifinals + Loser's Bracket Round 4 and 5
+February 5th until February 6th.
+### Finals + Loser's Bracket Round 6 and Finals
+February 12th until February 13th.
+### Grand Finals + Loser's Bracket Grand Finals
+February 19th until February 20th.`,
+    },
+    {
+      title: "Registration",
+      body:
+        "Click on the Discord button to join the server of our tournament, from there you can sign up as described in 'Tournament Registration and Teams Creation' in the rules. **Joining the Discord is mandatory to play in this tournament, you cannot sign up for the tournament if you don't do so.**",
+    },
+    {
+      title: "Prizes",
+      body: `
+- **1st place:** osu! profile badge `,
+    },
+  ],
+  rules: `
+# General Rules
+- This tournament is a **3v3 double-elimination tournament** with teams of **6 players maximum**. 
+- The players will play in a **double-elimination bracket**.
+- The Score System used will be **Score V2**.
+- Any player who meets the requirements stated in the rank restrictions part is allowed to sign up. Since this tournament will aim to get badged some players might be declined by osu! staff.
+- Any members of staff apart from **Organizers, Mappoolers, Mappers and Referees** are allowed to play.
+- Players and staff members must have read this ruleset entirely (and we will assume you did so throughout the tournament).
+- All participants must stay respectful and keep a proper attitude. Not following this rule can result in a ban/blacklist from the tournament. This rule concerns all the staff as well.
+- Any rule changes or unexpected occurrences will be announced in the Discord server.
+
+# Tournament Registration and Teams Creation
+ - In order to participate you need to need to be in a team of **3 to 6 players** of your choosing. There is no limit to who you can have on your team, with exception to the staff mentioned above and people who are already on another team.
+ - One member of the team will need to be the captain. This should be decided before you sign up.
+ - The captain is going to be the one to reschedule a match if needed and will ensure that their teammates are here for the matches. 
+ - To sign up, you or one of your teammates need to send a message in the #sgts-signup channel containing the following.
+   - A ping to @TaikoMom (or another host if unavailable)
+   - A list of your and your teammates' usernames, be sure to note who the captain is!
+   - Your teamname.
+   - A custom flag, a .png file of 70x47.
+ - TaikoMom (or another host if unavailable) will add your team to the site and give you access to a team chat. It might take some time before your sign-up is processed as this needs to be done manually.
+ - Teams will be numbered on order of signup. This number will be used for your team chat, as well as your teamname if you do not submit a custom teamname.
+
+# Stage Regulations
+- There are seven stages in the 3v3 tournament, the Qualifiers, the RO32, the RO16, the Quarterfinals, the Semifinals, the Finals, and the Grand Finals.
+- Players will be seeded with the average of their ranks on each map during the Qualifiers round.
+- The winning condition for each stage are listed below:
+    - Round of 32, Round of 16: Best-Of-9 (First to 5 points)
+    - Quarterfinals: Best-Of-11 (First to 6 points)
+    - Semifinals, Finals, and Grand Finals: Best-Of-13 (First to 7 points)
+   
+# Qualifiers Regulations
+ - During this round there will be multiple lobbies available at various hours.
+ - A private match will be created and team captains will be invited into the lobby when it's their time. Make sure your team is online at this time.
+ - There are no warmups.
+ - Seeding will be made with the average rank of your team's total score on each map. 
+ - Maps will be played twice in a row during the lobby. The best run will be taken into account for the ranking.
+ - Only the 32 best teams will continue on to the bracket stage.
+
+# Mappool Instructions
+- There will be 1 mappool for each of the following: Qualifiers, Round of 32, Round of 16, Quarterfinals, Semifinals, Finals, and Grand Finals. 
+- The Loser's Bracket will play on the same pool as the Winner's bracket of the same weekend.
+- The Qualifiers pool will be different from all the other rounds, as it will have a format of 3 NoMod maps, 2 Hidden maps, 2 HardRock maps, 2 DoubleTime maps, and a Hidden + HardRock map.
+- Each mappool consists of 6 brackets: **NoMod, Hidden, HardRock, DoubleTime, FreeMod, and Tiebreaker**.
+- The mappools from Round of 32 until Quarterfinals will have 15 maps each. The mappools from Semifinals until Grand Finals will have 19 maps each.
+- The size of the NoMod bracket will be of  5 from Round of 32 until Quarterfinals, and it will be of 6 in Semifinals until Grand Finals.
+- The size of the Hidden, Hard Rock, and Double Time will be of 2 from Round of 32 until Quarterfinals, and it will be of 3 from Semifinals until Grand Finals.
+- The size of the FreeMod pool will be of 3 during the rest of the tournament.
+- The FreeMod pool is played with FreeMod activated. Each team needs to have a minimum of 2 mods selected for FreeMod picks. This can be Hidden or Hard Rock and both mods can be selected by one player to satisfy the 2-mod requirement.
+- The Tiebreaker is played under FreeMod conditions but it is not necessary to enable mods.
+- The mappool for each stage will be showcased a week before.
+
+# Scheduling Instructions
+- Every stage will be held **during the weekend**. (it is however allowed to schedule on another day if no good time is found during the weekend and a referee is available)
+- We will try to schedule as best as possible to fit with the timezones. The schedule will be released the previous weekend of each stage.
+- Reschedules will only be considered if both players agree to a time, this needs to be done and notified to the tournament staff before **Thursday at 23:59 UTC** in that particular week when your match takes place.
+
+# Match Regulations
+- A private lobby will be created 10 minutes before your match by a referee. Both team captains will be invited into the room when it's match time. 
+- If no staff or referee is available the match will be postponed.
+- If a team has fewer than 3 players at match time, the maximum time the match can be postponed is 10 minutes.
+- There is no penalty for not joining the room instantly after match time as long as it's within the postponed time.
+- If neither of the teams have enough players in the lobby after 10 minutes, the highest seeded team will advance.
+- The match will be played as TeamVS and ScoreV2 and the name of the room must be: "SGTS 2021: (Team Red) vs (Team Blue)".
+- Each team is allowed one warm-up but it must be below 3 minutes, and must be sent to the referee and streamer of the match one hour prior to the match's start time.
+- If a team does not respect the time said in the rule above, they will not have any warm-up for their match.
+- Only the captain can give their team's picks, ban and roll to the referee.
+- Each team has to ban **one beatmap** to be selected from the pool (apart from Tiebreaker). These beatmaps are not allowed to be picked by either team in the entire match.
+- Each team captain will roll with the command "!roll" in the chat. Rolls from players other than the captain will be ignored.
+- The team with the **highest roll bans second and picks first**. The team with **the lowest roll bans first and picks second**.
+- Each team has 2 mins to announce their ban. If the captain didn't tell the referee by that time the ban will be made with a roll.
+- Beatmap selection will then alternate between each team selecting a beatmap out of the map pool.
+- Teams may pick freely from any bracket.
+- Teams will have 2 minutes to pick a beatmap and 2 minutes to get ready. If a team captain didn't tell their pick before the end of the timer the map will be picked with a roll. And if players do not get ready before the end of the timer, the match will be started, no matter what.
+- If a player disconnects between the beatmaps, another player form the team will need to take their spot.
+- If there is no other team member to replace the disconnected layer, the match can be delayed 10 minutes at maximum. This delay can only be applied once.
+- If a player disconnects after 30 seconds, they get treated as if they failed the map.
+- Disconnects within 30 seconds after beatmap begin can be rematched. This is only allowed once per team.
+- Failed players' scores do count.
+- If the beatmap ends in a draw the game will be nullified and the map will be replayed. If it does end in a tie again, the map will be nullified again, and the team who picked it will have to pick another map.
+- In case of a tie in points (example: 4-4 in BO9) the tiebreaker is selected.
+  `,
+};
