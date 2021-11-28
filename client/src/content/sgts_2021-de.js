4c36f03e9f768a04f41e489fc11f80ca1daaabfa
TsukoteAmie <thepeaceluffer@gmail.com>
2021-11-28 12:39:15 +0100

    German transl

---
+++ client/src/content/sgts_2021-de.js
@@ -0,0 +1,124 @@
+export default {
+  name: "Squad Global Taiko Showdown",
+  description:
+    "Der Squad Global Taiko Showdown ist unser 3-gegen-3 Turnier, in dem Spieler ihre eigenen Teams mit bis zu 6 Spielern selbst auswählen können. Nach der Qualifikation werden die besten 32 Teams in einem hitzigen Doppel-K.O.-System aufeinandertreffen.",
+  discord: "https://discord.gg/3mGC3HB",
+  homepage: [
+    {
+      title: "Einschränkungen",
+      body: `Für dieses Turnier gibt es weder Länder- noch Rangbeschränkungen. Jeder osu!taiko Spieler, der innerhalb der letzten 12 Monate eingeschränkt wurde, ist für dieses Turnier nicht zugelassen. Der osu! Support behält sich vor, jeden beliebigen Spieler nach der Screeningphase vom Turnier auszuschließen.`,
+    },
+    {
+      title: "Daten",
+      body: `
+### Anmeldephase
+27. November bis 12. Dezember.
+### Screeningphase
+12. Dezember bis 2. Januar.
+### Qualifikationen
+8. Januar bis 9. Januar.
+### Runde der letzten 32
+15. Januar bis 16. Januar.
+### Achtelfinale + Verliererbracket Runde 1
+22. Januar bis 23. Januar.
+### Viertelfinale + Verliererbracket Runde 2 und 3
+29. Januar und 30. Januar.
+###  Halbfinale + Verliererbracket Runde 4 und 5
+5. Februar bis 6. Februar.
+### Finale + Verliererbracket Runde 6 und Finale
+12. Februar und 13. Februar.
+### Großes Finale + Verliererbracket Großes Finale
+19. Februar und 20. Februar.`,
+    },
+    {
+      title: "Anmeldung",
+      body:
+        "Klicke auf die Discordfläche, um dem Turnierserver beizutreten. **Alle Teammitglieder müssen zwingend dem Discordserver beitreten, sonst kann das Team nicht am Turnier teilnehmen.** Der Teamkapitän registriert sein Team, nachdem er sich mit seinem osu! Account auf der Webseite eingeloogt hat.",
+    },
+    {
+      title: "Preise",
+      body: `
+- **1. Platz:** osu! Profilmedaille `,
+    },
+  ],
+  rules: `
+# Allgemeine Regeln
+- Dieses Turnier ist ein **3-gegen-3 Doppel-K.O.-Turnier** mit Teams aus jeweils **maximal 6 Spielern**.
+- Die Teams spielen in einem **Doppel-K.O.-System**.
+- Als Wertungssystem wird **Score V2** benutzt.
+- Jeder Spieler, der den oben genannten Anforderungen entspricht, darf sich anmelden. Da dieses Turnier das Ziel hat, eine Profilmedaille als Preis herauszugeben, kann es passieren, dass Spieler von den osu! Mitarbeitern abgelehnt werden.
+- Alle Mitarbeiter außer **Organisatoren, leitende Mappooler, Mapper und Schiedsrichter** dürfen am Turnier teilnehmen.
+- Spieler und Mitarbeiter müssen das Regelwerk in voller Länge gelesen haben (und wir werden während des Turniers davon ausgehen, dass dies getan wurde).
+- Alle Teilnehmer müssen sich respektvoll und angemessen verhalten. Verstöße gegen diese Regel können zu einem Ausschluss von diesem Turnier führen. Diese Regel betrifft auch sämtliche Mitarbeiter.
+- Regeländerungen und unerwartete Vorfälle werden im Discordserver verkündet.
+
+# Turnieranmeldung und Teambildung
+ - Um teilnehmen zu können, musst du dich in einem Team von **3 bis 6 Spielern** befinden.
+ - Einer der Teammitglieder muss als Teamkapitän ausgewählt werden. Dies sollte vor der Anmeldung geklärt werden.
+ - Teamkapitäne verlegen bei Bedarf Spielzeiten und stellen sicher, dass ihre Teammitglieder online sind für die Spiele.
+ - Um sich anzumelden, muss der Teamkapitän sein komplettes Team auf der Webseite registrieren nach folgenden Anweisungen:
+   - Der erste Spieler auf der Liste ist der Teamkapitän. Dieser Position wird automatisch mit dem Namen des Spielers besetzt, der das Team anmeldet, weshalb der Teamkapitän das Team anmelden muss.
+   - Ein benutzerdefinierter Teamname muss angegeben werden.
+   - Außerdem kann eine benutzerdefinierte Flagge mit den Maßen 70x47 als .png-Datei hinzugefügt werden. Solltet ihr keine Flagge haben, wird automatisch die Landesflagge des Teamkapitäns als Ersatz genommen.
+ - Ein Organisator wird deinem Team Rollen auf Discord zuweisen und euch einen Teamchat zur Verfügung stellen. Dies könnte jedoch etwas dauern, da dies manuell gemacht werden muss.
+
+# Regelungen zu den Phasen
+- Das Turnier ist in sieben Phasen eingeteilt: die Qualifikation, die Runde der letzten 32, das Achtelfinale, das Viertelfinale, das Halbfinale, das Finale und das Große Finale.
+- Die Teams werden anhand ihrer durchschnittlichen Platzierung in der Qualifikation eingeteilt.
+ - Die Siegesbedingungen für jede Phase lauten wie folgt:
+   - Runde der letzten 32, Achtelfinale: Best-of-9 (Gewinne 5 Maps)
+   - Viertelfinale: Best-of-11 (Gewinne 6 Maps)
+   - Halbfinale, Finale und Großes Finale: Best-of-13 (Gewinne 7 Maps)
+   
+# Regelungen zur Qualifikation
+ - Während dieser Phase werden mehrere Lobbys zu verschiedenen Zeiten zur Verfügung stehen. 
+ - Eine private Lobby wird erstellt und der Teamkapitän wird zur richtigen Zeit in die Lobby eingeladen. Stell sicher, dass dein Team während dieser Zeit online ist.
+ - Es gibt keine Warm-Ups.
+ - Seeds werden vergeben durch den durchschnittlichen Rang jedes Teams auf jeder Map. 
+ - Es wird zwei Durchläufe pro Map geben. Der beste Score auf einer Map wird für die Bewertung zählen. 
+ - Nur die besten 32 Teams rücken in die Runde der letzten 32 vor.
+
+# Anweisungen zum Mappool
+ - Es wird jeweils einen Mappool für folgende Phasen geben: Qualifikation, Runde der letzten 32, Achtelfinale, Viertelfinale, Halbfinale, Finale und Großes Finale.
+ - Das Verliererbracket wird mit dem gleichen Mappool wie das Gewinnerbracket in der jeweiligen Woche spielen.
+ - Der Qualifikations-Mappool unterscheidet sich von allen anderen Mappools, da er aus 3 NoMod-Maps, 2 Hidden-Maps, 2 HardRock-Maps und 2 DoubleTime-Maps und einer Hidden- und HardRock-Map besteht.
+ - Der Mappool besteht aus 6 Formaten: **NoMod, Hidden, HardRock, DoubleTime, FreeMod und dem Tiebreaker**. 
+ - Die Mappools für die Runde der letzten 32-, Achtelfinale- und Viertelfinale-Mappools beinhalten jeweils 15 Maps. Die Halbfinale-, Finale- and Großes Finale-Mappools bestehen aus jeweils 19 Maps.
+ - Die Größe des NoMod-Formats beläuft sich auf 5 Maps in der Runde der letzten 32 bis zum Viertelfinale und 6 Maps ab dem Halbfinale bis zum Großen Finale.
+ - Die Größe des Hidden-, HardRock- und DoubleTime-Formats beläuft sich auf 2 Maps von der Runde der letzten 32 bis zum Viertelfinale und auf 3 Maps im Halbfinale, Finale und Großen Finale.
+ - Die Größe des FreeMod-Formats beläuft sich auf 3 Maps in allen Phasen.
+ - Das FreeMod-Format wird mit FreeMod gespielt. Jedes Team muss mindestens zwei Mods ausgewählt haben für die FreeMod-Picks. Dabei kann entweder Hidden oder HardRock gewählt werden. Ein Spieler kann auch Hidden- und HardRock auswählen, um die Bedingung zu erfüllen.
+ - Der Tiebreaker wird unter FreeMod-Bedingungen gespielt, jedoch ist kein Spieler gezwungen einen Mod zu benutzen.
+ - Die jeweiligen Mappools werden eine Woche im Voraus per Liveübertragung bekannt gegeben.
+
+# Anweisungen zu den Spielzeiten
+ - Jede Phase wird **am Wochenende** abgehalten. (Es ist erlaubt, das Spiel auf einen anderen Wochentag zu verlegen, wenn keine Einigung zwischen den Spielern erzielt werden konnte. Ein Schiedsrichter muss jedoch verfügbar sein für die vereinbarte Zeit.) 
+ - Wir werden versuchen die Spielzeiten so gut wie möglich an die jeweiligen Zeitzonen der Teams anzupassen. Die Spielzeiten werden eine Woche vor Spielbeginn bekannt gegeben. 
+ - Das Verlegen der Spielzeit ist nur möglich, wenn sich beide Teams auf eine neue Zeit einigen können. Ein Beweis muss den Mitarbeitern bis spätestens **Donnerstag, 23:59 UTC+0** (gleiche Woche wie das betreffende Spiel) vorgelegt werden, damit die Verlegung gültig ist.
+
+# Regelungen zum Spiel
+- Eine private Lobby wird 10 Minuten vor Spielbeginn von einem Schiedsrichter erstellt. Beide Teamkapitäne werden zur richtigen Zeit in die Lobby eingeladen.
+- Falls kein Mitarbeiter oder Schiedsrichter verfügbar ist, wird das Spiel nach hinten verlegt. 
+- Falls in einem Team weniger als 3 Spieler anwesend sind, kann das Spiel um maximal 10 Minuten verzögert werden. 
+- Verspätungen werden nicht bestraft, solange sie im Rahmen der 10-minütigen Verzögerung stattfinden.
+- Sollten beide Teams nicht anwesend sind, rückt das Team mit dem höheren Seed vor.
+- Das Spiel wird in TeamVS und ScoreV2 ausgetragen und der Name der Lobby lautet "SGTS 2021: (Rotes Team) vs (Blaues Team)".
+ - Jedes Team darf ein Warm-Up wählen, es muss jedoch unter 3 Minuten lang sein. Das Warm-Up muss an den zuständigen Schiedsrichter und Streamer eine Stunde vor Spielbeginn gesendet werden.
+ - Falls ein Team das oben genannte Zeitlimit nicht respektiert, hat es kein Recht auf ein Warm-Up.
+ - Der Schiedsrichter wertet nur den Ban, Pick und Roll des Teamkapitäns als gültige Eingabe.
+ - Jedes Team muss genau **eine Beatmap** aus dem Pool verbieten (außer des Tiebreakers). Diese Beatmap kann man während des gesamten Spiels nicht wählen. 
+ - Jeder Teamkapitän würfelt mit dem Befehl "!roll" im Chat. 
+ - Das Team mit dem höheren Wurf **verbietet als Zweites und wählt als Erstes aus**. Das Team mit dem niedrigeren Wurf **verbietet als Erstes und wählt als Zweites**. 
+ - Jeder Teamkapitän hat 2 Minuten Zeit, um seinen Ban zu verkünden. Falls kein Ban in diesem Zeitfenster mitgeteilt wurde, würfelt der Schiedsrichter einen Ban. 
+ - Beatmaps werden danach abwechselnd von beiden Teams aus dem Mappool gewählt. 
+ - Die Teams können ohne Einschränkungen von jedem Bracket wählen. 
+ - Die Teams haben 2 Minuten Zeit, um eine Beatmap zu wählen und weitere 2 Minuten, um sich vorzubereiten. Falls keine Wahl in diesem Zeitfenster mitgeteilt wurde, würfelt der Schiedsrichter eine Beatmap. Das Spiel wird gestartet, sobald der Timer abgelaufen ist, auch wenn die Spieler nicht bereit sind.
+ - Falls die Verbindung bei einem Spieler zwischen den Beatmaps abbricht, muss das Team einen Ersatzspieler einwechseln.
+ - Falls kein Ersatzspieler verfügbar ist, kann das Spiel maximal 10 Minuten verzögert werden. Diese Ausnahme kann nur einmal pro Spiel angewendet werden.
+ - Falls die Verbindung bei einem Spieler 30 Sekunden nachdem die Beatmap gestartet wurde, abbricht, wird der Score des betroffenen Spielers nicht zum Gesamtscore des Teams gezählt.
+ - Verbindungsabbrüche in den ersten 30 Sekunden nachdem die Beatmap gestartet wurde, erlauben ein Wiederholen der Beatmap. Diese Ausnahme kann nur einmal pro Spiel angewendet werden. 
+ - Gescheiterte Scores zählen.
+ - Falls eine Beatmap unentschieden ausgeht, wird die betreffende Runde nicht gewertet und die Beatmap wird wiederholt. Passiert dies erneut, wird die Runde nicht gewertet und das Team, das die Map gewählt hat, muss eine andere Map auswählen.
+ - Falls es zu einem Unentschieden im Punktestand kommt, (Beispiel: 4-4 in einem BO9) wird der Tiebreaker gewählt.
+  `,
+};
