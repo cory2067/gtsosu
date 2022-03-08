b0089cac5629e176f3682aa57636f1a2686008dd
Cory Lynch <cjl2625@gmail.com>
2022-03-07 18:46:53 -0800

    Refactor UserModal to a functional component

--- client/src/components/modules/UserModal.js
+++ client/src/components/modules/UserModal.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 import "../../utilities.css";
 
-import { Form, Select, Input, Modal, InputNumber } from "antd";
+import { Form, Select, Input, Modal } from "antd";
 import ContentManager from "../../ContentManager";
 
 const UI = ContentManager.getUI().userSettings;
@@ -15,56 +15,42 @@ const layout = {
   },
 };
 
-class UserModal extends Component {
-  constructor(props) {
-    super(props);
+const timezones = [];
+for (let i = -12; i <= 14; i += 0.5) {
+  timezones.push(i);
+}
 
-    const range = [];
-    for (let i = -12; i <= 14; i += 0.5) {
-      range.push(i);
-    }
-    this.range = range;
-  }
+function UserModal({ user, visible, loading, handleOk, handleCancel, onValuesChange }) {
+  return (
+    <Modal
+      title={`Settings for ${user.username}`}
+      visible={visible}
+      confirmLoading={loading}
+      onOk={handleOk}
+      onCancel={handleCancel}
+    >
+      <Form {...layout} onValuesChange={onValuesChange} initialValues={user}>
+        <div style={{ marginBottom: 12 }}>{UI.note}</div>
+        <Form.Item name="discord" label={UI.discord}>
+          <Input />
+        </Form.Item>
+        <Form.Item name="timezone" label={UI.timezone}>
+          <Select placeholder="UTC+0">
+            {timezones.map((num) => (
+              <Select.Option key={num} value={num}>
+                {`UTC${num >= 0 ? "+" : ""}${num}`}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
+      </Form>
 
-  render() {
-    return (
-      <Modal
-        title={`Settings for ${this.props.user.username}`}
-        visible={this.props.visible}
-        confirmLoading={this.props.loading}
-        onOk={this.props.handleOk}
-        onCancel={this.props.handleCancel}
-      >
-        <Form
-          {...layout}
-          onValuesChange={this.props.onValuesChange}
-          initialValues={this.props.user}
-        >
-          <div style={{ marginBottom: 12 }}>{UI.note}</div>
-          <Form.Item name="discord" label={UI.discord}>
-            <Input />
-          </Form.Item>
-          <Form.Item name="timezone" label={UI.timezone}>
-            <Select placeholder="UTC+0">
-              {this.range.map((num) => (
-                <Select.Option key={num} value={num}>
-                  UTC{num >= 0 && "+"}
-                  {num}
-                </Select.Option>
-              ))}
-            </Select>
-          </Form.Item>
-        </Form>
-
-        <div>
-          <span className="u-bold">{UI.tournies}: </span>
-          {this.props.user.tournies && this.props.user.tournies.length
-            ? this.props.user.tournies.join(", ")
-            : "none"}
-        </div>
-      </Modal>
-    );
-  }
+      <div>
+        <span className="u-bold">{UI.tournies}: </span>
+        {user.tournies && user.tournies.length ? user.tournies.join(", ") : "none"}
+      </div>
+    </Modal>
+  );
 }
 
 export default UserModal;
