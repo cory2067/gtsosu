d3e28642d39cd3f107aae4e30a035e33e19be653
Cory Lynch <cjl2625@gmail.com>
2022-03-07 19:20:01 -0800

    Begin migrating server to typescript

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
-const express = require("express");
+import express from "express";
+//const express = require("express");
 const path = require("path");
 const logger = require("pino")();
 const passport = require("passport");
