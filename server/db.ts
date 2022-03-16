49180ae7a4117084d03d84c32e7cb285a93c38e9
Cory Lynch <cjl2625@gmail.com>
2022-03-16 01:37:11 -0700

    Finish migrating db.ts

--- server/db.ts
+++ server/db.ts
@@ -6,16 +6,13 @@ const srv =
   process.env.NODE_ENV === "production" ? process.env.MONGO_SRV : process.env.DEV_MONGO_SRV;
 
 export default {
-  init: () => {
-    // connect to mongodb
-    return mongoose
-      .connect(srv, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-      })
-      .then(() => logger.info("Server connected to MongoDB"))
-      .catch((err) => logger.error("Error connecting to MongoDB", err));
+  init: async () => {
+    try {
+      await mongoose.connect(srv);
+      logger.info("Server connected to MongoDB");
+    } catch (err) {
+      logger.error("Error connecting to MongoDB", err);
+    }
   },
   getConnection: () => mongoose.connection,
 };
