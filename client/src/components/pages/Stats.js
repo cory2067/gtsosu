bb5ea1568690b786f4789e3ada23b1c2abfd7cd0
Chupalika <Chupalika@gmail.com>
2022-03-26 18:46:20 -0400

    pass in tourney to post/stage-stats
    - fixes authentication error hopefully
    - also I messed up seed calc again

--- client/src/components/pages/Stats.js
+++ client/src/components/pages/Stats.js
@@ -227,7 +227,7 @@ export default function Stats({ tourney, user }) {
   };
 
   const submitEditedStageStats = async () => {
-    const updatedStats = await post("/api/stage-stats", { stats: state.stageStatsEdit });
+    const updatedStats = await post("/api/stage-stats", { tourney, stats: state.stageStatsEdit });
     setState({
       ...state,
       stageStats: updatedStats,
@@ -429,12 +429,12 @@ export default function Stats({ tourney, user }) {
   };
 
   const getTeamSeed = (rank) => {
-    const rangeSize = Math.pow(2, Math.floor(Math.sqrt(state.overallTeamStats.length))) / 4;
+    const rangeSize = Math.pow(2, Math.floor(Math.log2(state.overallTeamStats.length))) / 4;
     return Math.ceil(rank / rangeSize);
   };
 
   const getPlayerSeed = (rank) => {
-    const rangeSize = Math.pow(2, Math.floor(Math.sqrt(state.overallPlayerStats.length))) / 4;
+    const rangeSize = Math.pow(2, Math.floor(Math.log2(state.overallPlayerStats.length))) / 4;
     return Math.ceil(rank / rangeSize);
   };
 
