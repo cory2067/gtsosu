b24bb8e932d636df5c423223a1713ffd66a3f98f
vun <vunyunt@gmail.com>
2022-03-29 11:00:12 +0800

    Replace trace with info/warn

--- server/permissions/contexts/TeamContext.ts
+++ server/permissions/contexts/TeamContext.ts
@@ -3,6 +3,9 @@ import user, { IUser } from "../../models/user";
 import { Populate } from "../../types";
 import { UserRole } from "../UserRole";
 import { PermissionContext } from "./context";
+import pino from "pino";
+
+const logger = pino();
 
 export class TeamContext implements PermissionContext {
   private team: Populate<ITeam, PopulatedTeam>;
@@ -12,6 +15,7 @@ export class TeamContext implements PermissionContext {
   }
 
   public hasRole(user: IUser, role: UserRole) {
+    logger.info({ context: "TeamContext.hasRole", team: this.team, user, role });
     switch (role) {
       case UserRole.Captain:
         return this.team.players[0].username === user.username;
