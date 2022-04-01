47ba423aac8b77696fff28982634e66eeda4dfc4
vun <vunyunt@gmail.com>
2022-04-01 12:00:23 +0800

    Migrate ensure

--- server/ensure.ts
+++ server/ensure.ts
@@ -1,5 +1,7 @@
 // Ensure a user has a certain permission before allowing access
 import pino from "pino";
+import { UserAuth } from "./permissions/UserAuth";
+import { UserRole } from "./permissions/UserRole";
 const logger = pino();
 
 function loggedIn(req, res, next) {
@@ -10,9 +12,15 @@ function loggedIn(req, res, next) {
   next();
 }
 
-// ensure the user is an admin, or otherwise has one of the roles specified in userRoles
-function ensure(userRoles, title) {
-  const roles = ["Host", "Developer", ...userRoles];
+/**
+ * @deprecated Use UserAuth directly instead
+ *
+ * ensure the user is an admin, or otherwise has one of the roles specified in userRoles
+ * @param userRoles
+ * @param title
+ * @returns
+ */
+function ensure(userRoles: UserRole[], title: string) {
   return (req, res, next) => {
     if (!req.user || !req.user.username) {
       return res.status(401).send({ error: "Not logged in, refusing access." });
@@ -20,11 +28,11 @@ function ensure(userRoles, title) {
 
     const tourney = req.body.tourney || req.query.tourney;
 
-    if (
-      req.user.admin ||
-      req.user.roles.some((r) => roles.includes(r.role) && r.tourney === tourney)
-    ) {
-      return next();
+    if (tourney) {
+      const auth = new UserAuth(req.user).forTourney(tourney);
+      if (auth.hasAnyRole(userRoles)) {
+        return next();
+      }
     }
 
     logger.warn(`${req.user.username} attempted to gain ${title} access!`);
@@ -32,11 +40,20 @@ function ensure(userRoles, title) {
   };
 }
 
+// Do we wanna define sets of user roles (like pooler) as constants UserRoles instead?
 export default {
+  /** @deprecated */
   isAdmin: ensure([], "admin"),
-  isPooler: ensure(["Mapsetter", "All-Star Mapsetter", "Head Pooler", "Mapper"], "pooler"),
-  isRef: ensure(["Referee"], "ref"),
-  isStreamer: ensure(["Streamer"], "streamer"),
-  isCommentator: ensure(["Commentator"], "commentator"),
+  /** @deprecated */
+  isPooler: ensure(
+    [UserRole.Mapsetter, UserRole.AllStarMapSetter, UserRole.HeadPooler, UserRole.Mapper],
+    "pooler"
+  ),
+  /** @deprecated */
+  isRef: ensure([UserRole.Referee], "ref"),
+  /** @deprecated */
+  isStreamer: ensure([UserRole.Streamer], "streamer"),
+  /** @deprecated */
+  isCommentator: ensure([UserRole.Commentator], "commentator"),
   loggedIn,
 };
