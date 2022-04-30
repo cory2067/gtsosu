06410a8862a99436ea286f946545f1e084910423
Kasumiikun <kasumiikun@gmail.com>
2022-04-30 02:17:19 +0200

    Release French Translation for AGTS 2022.

---
+++ client/src/content/agts_2022-fr.js
@@ -0,0 +1,152 @@
+export default {
+  name: "Advanced Global Taiko Showdown",
+  description:
+    "L'Advanced Global Taiko Showdown, notre tournoi en 2v2 pour les joueurs de niveau avancé, restrient aux joueurs compris entre les rangs de #500 à #1500. Les 32 meilleurs équipes après les qualifications s'affronteront dans une phase de poules, où 16 équipes en sortiront pour jouer dans un arbre à double élimination.",
+  discord: "https://discord.gg/3mGC3HB",
+  homepage: [
+    {
+      title: "Restrictions",
+      body: `Ce tournoi est ouvert aux joueurs compris entre les rangs #500 à #1,500. N'importe quel joueur dans cette limite de rangs qui n'a pas été restrict dans les 12 derniers mois sera autorisé à participer à ce tournoi. Le support d'osu! se réserve le droit de retirer n'importe quel joueur de la compétition à la suite de la phase de screening.`,
+    },
+    {
+      title: "Dates",
+      body: `
+### Phase d'inscriptions
+30 avril jusqu'au 15 mai.
+### Phase de screening
+15 mai jusqu'au 22 mai.
+### Qualifications
+28 mai jusqu'au 29 mai.
+### Phase de poules
+4 juin jusqu'au 5 juin.
+### Huitièmes de finale
+11 juin jusqu'au 12 juin.
+### Quarts de finale + Premier tour du Loser's Bracket
+18 juin jusqu'au 19 juin.
+### Demi-finales + Deuxième et troisième tour du Loser's Bracket
+25 juin jusqu'au 26 juin.
+### Finale du Winner's Bracket + Quatrième et cinquième tour du Loser's Bracket
+2 juillet jusqu'au 3 juillet.
+### Grande Finale + Finale du Loser's Bracket et All-Stars
+9 juillet jusqu'au 10 juillet.`,
+
+    },
+    { title: "Registration", body: "Cliquez sur le bouton S'inscrire ci-dessus une fois connecté avec votre compte osu!, puis cliquez sur le bouton Discord pour rejoindre le serveur Discord de notre tournoi. **Rejoindre le serveur Discord est obligatoire pour prendre part à la compétition, et vous serez retirés du tournoi si vous ne le faites pas.**" }, 
+    {
+      title: "Prizes",
+      body: `
+- **1ère place:** Badge sur le profil osu!. (à confirmer)`,
+    },
+  ],
+  rules: `
+  # Règles Générales
+ - Ce tournoi est joué en **équipes de 2 contre 2** avec des équipes à **5 joueurs maximum**.
+ - Les équipes passeront tout d'abord une phase de **qualifications**. Elles joueront ensuite dans une **phase de poules** et enfin dans un **arbre à double élimination**.
+ - Le système de score utilisé sera **Score V2**.
+ - N'importe quel joueur respectant les consignes listées dans la section Restriction de rang est autorisé à s'inscire. Puisque ce tournoi vise à récompenser les gagnants d'un badge, certains joueurs peuvent se voir refuser le droit de jouer par le staff d'osu!.
+ - N'importe quel membre du staff peut jouer, à l'exception des **Organiseurs, Mappoolers et Arbitres**.
+ - Les joueurs ainsi que les membres du staff doivent avoir lu ce règlement dans son entièreté (et nous partirons du principe que c'est le cas pendant le tournoi).
+ - Tous les participants doivent se montrer respecteux et garder une attitude correcte. Ne pas suivre cette règle peut entraîner une mise sur liste noire (blacklist) ou un bannissement du tournoi. Cette règle concerne également l'intégralité des membres du staff.
+ - N'importe quel changement dans les règles ou situation exceptionnelle sera annoncé(e) sur le serveur Discord.
+
+# Inscription au tournoi et Création des équipes
+ - Afin de participer à ce tournoi, vous devez avoir un rang compris entre #500 et #1500.
+ - Les joueurs s'inscrivent individuellement. Lorsque leur inscription est acceptée, ils reçevront le rôle de leur pays.
+ - Le staff peur refuser la participation d'un joueur au tournoi si ils jugent que son niveau est bien au dessus de celui de l'audience ciblée. 
+ - Cette année, nous autorisons les pays à avoir jusqu'à deux équipes, si les conditions suivantes sont respectées :
+   - Il y a **7 joueurs ou plus** inscrits pour un pays.
+   - Chaque équipe doit avoir **au moins 3 joueurs**.
+ - A cause de la règle précédente, il existera plusieurs situations lors de la création des équipes: 
+   - Si il y a 5 joueurs ou moins inscrits pour un pays, ces joueurs vont former l'équipe représentant leur pays. 
+   - Dans le cas où il y a plus de 10 joueurs inscrits pour un pays, les joueurs décideront entre eux-mêmes qui prendra part au tournoi. Si ils ne parviennent pas à se décider, les 10 meilleurs joueurs au classement seront sélectionnés pour former les deux équipes. 
+   - Si il y a 6 joueurs inscrits, les joueurs devront choisir les 5 joueurs qui participeront au tournoi. Si ils n'y parviennent pas, les 5 meilleurs joueurs au classement seront sélectionnés pour former l'équipe. 
+   - Si il y a de 7 à 10 joueurs (après avoir gardé les mieux classés) inscrits pour un pays, les joueurs de ce pays auront quelques jours pour décider de la manière dont ils vont former les deux équipes. 
+   Il est recommandé de trouver un accord aussi rapidement que possible pour ensuite décider du capitaine de l'équipe afin de pouvoir plannifier et préparer vos matchs rapidement. 
+   - Dans le cas où les joueurs ne parviennent pas à trouver un terrain d'entente, ils seront classés par points de performance et répartis dans les deux équipes avec au moins 3 membres dans chacune d'elles, avec les plus hauts dans une équipe A et les plus bas dans une équipe B. 
+   - Les joueurs décideront du capitaine de leur équipe. 
+   - Si les joueurs ne sont pas d'accord sur un capitaine ou si il n'y en a pas, le joueur le plus haut au classement sera choisi pour être capitaine.
+ - Le capitaine est celui qui sera en charge de changer l'horaire d'un match si nécessaire, et de s'assurer que ses coéquipiers soient là pour les matchs. 
+
+# Règles des rounds
+ - Ce tournoi sera joué sur sept rounds : les Qualifications, la Phase de Poules, les Huitièmes de finale, les Quarts de finale, les Demi-finales, les Finales et les Grandes Finales. 
+ - Les équipes seront seedées selon leur moyenne de rang sur chaque beatmap pendant les qualifications en 4 niveaux de seed : Top seed, High seed, Low seed et Unseeded (Le processus de seeding est expliqué plus en détails dans la partie Règles des Qualifications). 
+ - Les équipes seront réparties dans **8 groupes de 4 équipes** pendant une retransmission en direct, qui aura lieu le Dimanche 29 mai à 18:00UTC (20h heure de Paris). 
+ - Pendant la Phase de Poules, toutes les équipes d'un même groupe se recontrent entre elles. Les **2 meilleures équipes** sortiront de leur groupe. 
+ - Le classement de chaque groupe est déterminé en fonction des résultats de chaque équipe selon l'ordre de priorité suivant : 
+    - Le plus de matchs remportés.
+    - Le plus haut ratio (nombre de beatmaps remportées - nombre de beatmaps perdues).
+    - Le plus de beatmaps remportées.
+    - Gagnant du match joué au préalable entre les deux équipes à égalité.
+ - Pendant la phase de Poules, une 'Victoire par défaut' sera considérée comme une victoire à 5:0, avec une différence de score à +1.0.
+ - La condition de victoire pour chaque round est la suivante :
+   - Phase de Poules : Best-Of-9 (remporter 5 points)
+   - Huitièmes, Quarts de Finale + Premier et deuxième tour de Loser's Bracket : Best-Of-11 (remporter 6 points)
+   - Demi-finales, Finales, Grandes Finales + Troisième et quatrième tour du Loser's Bracket : Best-Of-13 (remporter 7 points)
+
+# Règlement des Qualifications
+ - Pendant ce round, il y aura plusieurs salons multijoueur ouverts à divers horaires. 
+ - Un salon privé sera créé et les équipes seront invitées dans le salon lorsqu'il sera l'heure. Assurez-vous d'être en ligne à ce moment. 
+ - Il n'y a pas de beatmap d'échauffement.
+ - Les scores fail comptent.
+ - Seuls deux joueurs d'une même équipe joueront une beatmap. Il n'y a pas de limite de changement de joueurs entre les beatmaps.
+ - Le seeding sera réalisé avec la moyenne de rang de chaque équipe sur chaque beatmap. 
+ - Les beatmaps seront jouées deux fois pendant le salon multijoueur. Le meilleur des deux essais sera pris en compte pour le classement. 
+ - Seules les 32 meilleures équipes avanceront à la phase de Poules. 
+
+# Règlement des matchs
+ - Un salon multijoueur privé sera créé 10 minutes avant votre match par un arbitre. Les capitaines de chaque équipe seront invités à rejoindre le salon à l'heure du match. Puis l'arbitre ou les capitaines inviteront les autres joueurs dans le salon. 
+ - Si il n'y a pas de membre du staff ou d'arbitre dispoible, le match sera reporté. 
+ - Dans le cas où le nombre de joueurs présents n'est pas suffisant, la durée maximum de report d'un match est de 10 minutes. 
+ - Il n'y a pas de pénalité pour ne pas avoir rejoint le salon directement à l'heure du match tant que vous rejoignez dans le temps imparti. 
+ - Si aucune des deux équipes n'a le nombre de joueurs nécessaire pour jouer après 10 minutes, le match sera considéré comme une défaite pour les deux équipes en phase de poules. Dans l'arbre à double élimination, l'équipe avec le plus haut seed gagnera le match. 
+ - Les matchs seront joués en format TeamVs et ScoreV2, et le nom du salon doit être : "AGTS 2022: (Equipe Rouge) vs (Equipe Bleue)". 
+ - Chaque équipe à le droit à une beatmap d'échauffement mais celle-ci doit avoir une durée de moins de 3 minutes, et être envoyée sur le site Internet jusqu'à une heure avant le début du match. Les équipes seront autorisées à envoyer leur beatmap d'échauffement à partir du jour de la sortie des horaires. Sinon, l'équipe n'aura pas d'échauffement.
+ - Chaque équipe doit bannir **une beatmap** du mappool (à l'exception du Tiebreaker). Ces beatmaps ne peuvent pas être sélectionnées par aucune équipe pendant l'entièreté du match. 
+ - Le capitaine de chaque équipe doit faire un jet de dés avec la commande "!roll" dans le chat du salon multijoueur. 
+ - L'équipe qui fait le jet le plus haut **bannit en deuxième et choisit la première beatmap jouée**. L'équipe qui fait le jet le plus bas **bannit en premier et choisira la deuxième beatmap jouée**. 
+ - Chaque capitaine a 2 minutes pour annoncer le ban de son équipe. Si il n'annonce pas le ban de son équipe, l'arbitre décidera du ban de l'équipe via un jet de dés à la fin du temps imparti. 
+ - La sélection de beatmaps jouées alternera entre chaque équipe, qui choisira une beatmap du mappool chacune à son tour. 
+ - Les équipes peuvent choisir une beatmap indépendament du mod joué sur la beatmap. 
+ - Les équipes auront 2 minutes pour annoncer leur choix de beatmap et 2 minutes pour se mettre prêt. Si une équipe ne choisit pas de beatmap à la fin du temps imparti, l'arbitre décidera de la sélection de l'équipe via un jet de dés. 
+ - Interchanger entre les joueurs d'une même équipe pendant un match est autorisé, sans aucune restriction. 
+ - Si un joueur se déconnecte entre deux beatmaps jouées et son équipe ne peut pas faire remplacer ce joueur, le match peut être repoussé de 10 minutes au maximum. Ce délai n'est applicable qu'une seule fois. 
+ - Si un joueur se déconnecte pendant une beatmap, son score sera traité comme si il avait fail la beatmap. 
+ - Les déconnexions pendant les 30 premières secondes après le lancement d'une beatmap résulte en un relancement de la beatmap. Ceci est autorisé une seule fois par équipe. 
+ - Le score d'un joueur qui a fail une beatmap est comptabilisé et s'ajoute au score total de l'équipe. 
+ - Si une beatmap se termine sur une égalité, le point sera nullifié et la beatmap sera rejouée. 
+ - Dans le cas d'une égalité aux points (par exemple, 4-4 dans un match Best-Of-O9), le tiebreaker sera sélectionné. 
+
+# Règles des mappools
+ - Il y aura un mappool pour chacun des rounds suivants : Qualifications, Phase de Poules, Huitièmes de finale, Quarts de Finale, Demi-finales, Finales et Grandes Finales. L'événement All-Stars sera joué sur le mappool des Grandes Finales. 
+ - Le mappool des Qualifications sera différent de celui des autres rounds, puisqu'il suivra le format suivant : 3 beatmaps jouées en NoMod, 2 beatmaps jouées en Hidden, 2 beatmaps jouées en Hard Rock, 1 beatmap jouée en Double Time, et 1 beatmap jouée en FreeMod. 
+ - Les rounds de Loser's Bracket joueront sur le même mappool de que le round de Winner's Bracket de la même semaine. 
+ - Chaque mappool aura 6 catégories de mod: NoMod, Hidden, HardRock, DoubleTime, FreeMod et Tiebreaker. 
+ - Le mappool de la phase de Poules sera composés de 14 beatmaps, les mappools des Huitièmes et Quarts de finale seront composés de 15 beatmaps, et les mappools des Demi-finales, Finales et Grandes Finales seront composés de 16 beatmaps. 
+ - La taille des catégories Hidden, HardRock et DoubleTime sera de 2 beatmaps pour tous les rounds et de 3 beatmaps pour la catégorie FreeMod, pour tous les rounds également. 
+ - Les maps de la catégorie FreeMod sont jouées avec FreeMod activé. Chaque joueur peut individuellement choisir de jouer avec **Hidden, HardRock, ou les deux**. **Au moins un joueur par équipedoit avoir au moins un mod activé**. 
+ - Les joueurs doivent sélectionner différentes combinaisons de mods. Les deux joueurs ne peuvent pas avoir deux Hidden, deux HardRock deux Hidden+HardRock actifs en même temps.
+ - La taille de la catégorie NoMod sera de 4 beatmaps en phase de Poules, 5 beatmaps en Huitièmes et Quarts de Finale, et 6 beatmaps pour les Demi-finales, Finales et Grandes Finales.
+ - Le Tiebreaker est joué en condition de pick Freemod, mais il n'est pas nécessaire qu'au moins un joueur ait un mod actif. 
+ - Le mappool de chaque round sera révélé en direct une semaine avant le début des matchs joués sur ledit mappool. 
+
+# Règles des horaires
+ - Chaque round est joué **pendant le week-end**. (Il est cependant possible de décaler un horaire à un autre jour si aucun accord n'a été trouvé pour le weekend) 
+ - Plusieurs matchs peuvent se superposer, si des arbitres sont disponibles. 
+ - Nous essayerons de plannifier vos matchs du mieux possible en accord avec votre fuseau horaire. Le planning des matchs d'une semaine sera publié le week-end avant le début d'un round. 
+ - Les changements d'horaires ne seront acceptés que si les deux équipes sont en accord sur un même horaire. Une preuve doit être envoyée au staff du tournoi avant le **jeudi à 23:59 UTC+0** de la semaine pendant laquelle le match est joué. 
+ - Les changements d'horaires ne peuvent être effectués que par **un capitaine d'équipe**.
+
+# Règles All-Stars
+ - Le All-Stars est un événement pour le fun se déroulant la même semaine que les Grandes Finales ! 
+ Il montrera les meilleurs joueurs du tournoi jouant les uns contre les autres lors d'un match spécial. 
+ - 8 joueurs participeront à cet événement. Ils seront décidés par un vote. 
+ - Les deux joueurs les plus votés seront capitaines de chaque équipe. Leurs coéquipiers seront déterminés par le classement au vote des 6 joueurs suivants. Le joueur ayant été le plus voté jouera avec les joueurs dont le classement est un nombre impair, et le deuxième joueur le plus voté jouera avec les joueurs dont le classement est un nombre pair. 
+ - Les 8 joueurs ayant amassé le plus de votes joueront le All-Stars. Si il y a plus de 2 joueurs d'un même pays parmi les 8 joueurs les plus votés, seuls les deux mieux placés au vote participeront au All-Stars. 
+ - L'événement sera joué sous le format d'un match en 4 contre 4 avec les règles suivantes : 
+   - Best-Of-15. 
+   - Le mappool joué sera celui des Grandes Finales.
+   - Pas de bannissement de beatmaps. 
+   - La sélection de beatmaps se fait comme lors d'un match normal. 
+   - Les règles en cas de déconnexion lors d'un match normal s'appliquent ici aussi. 
+   `,
+};
