d442d1bdb999d50a722dfcbbd25780b2c50cfd7b
Kasumiikun <kasumiikun@gmail.com>
2022-11-28 07:37:56 +0100

    i'm sorry i legitimately cannot read (year change on suiji)

--- client/src/content/tsc_2023-en.js
+++ client/src/content/tsc_2023-en.js
@@ -1,5 +1,5 @@
 export default {
-  name: "Taiko Suiji Cup 2021",
+  name: "Taiko Suiji Cup 2023",
   description:
     "The Taiko Suiji Cup 2023, one of the biggest osu!taiko tournaments. 4v4, no rank limit, and entirely random teams. The top 32 teams after the seeding phase will play in a double-elimination bracket.",
   discord: "https://discord.gg/yV3bDTC",
