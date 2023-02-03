2d37928ceb7c268d80cbc1c217546b0962619c11
vun <vunyunt@gmail.com>
2023-02-03 19:28:23 +0800

    Further changes

--- client/src/components/pages/home/HomeBanner.js
+++ client/src/components/pages/home/HomeBanner.js
@@ -5,11 +5,15 @@ import stripesTopRight from "./svg/stripes-top-right.svg";
 import stripedCircleWhite from "./svg/striped-circle-white.svg";
 import stripedCircleOrange from "./svg/striped-circle-orange.svg";
 
+// import gtsLogo from "../../../public/gts-osu.svg";
+
 import "./HomeBanner.css";
+import { Button, Typography } from "antd";
+import { useMatchMedia } from "../../../utilities";
 
 function HomeBannerBackground() {
   return (
-    <div className="u-rounded-border HomeBannerBackground-container">
+    <div className="HomeBannerBackground-container">
       <img src={stripesBottomLeft} className="HomeBannerBackground-stripes-left" />
       <img src={stripesTopRight} className="HomeBannerBackground-stripes-right" />
       <img src={stripedCircleWhite} className="HomeBannerBackground-striped-circle-white" />
@@ -18,10 +22,42 @@ function HomeBannerBackground() {
   );
 }
 
-export default function HomeBanner() {
+function LoginButtons() {
   return (
-    <div className="u-rounded-border HomeBanner-container">
+    <div className="HomeBanner-login-buttons-container">
+      <Button type="primary" size="large">
+        Login
+      </Button>
+      <Button size="large">Learn More</Button>
+    </div>
+  );
+}
+
+export default function HomeBanner() {
+  var mobileLayout = useMatchMedia("(max-width: 1000px)")?.matches;
+
+  return (
+    <div className="HomeBanner-container">
       <HomeBannerBackground />
+      <div className="HomeBanner-section-1">
+        <div className="HomeBanner-section-1-inner">
+          <div className="u-flexRow u-flex-alignCenter">
+            <img src="public/gts-osu.svg" className="HomeBanner-logo" />
+            <Typography className="HomeBanner-title">GLOBAL TAIKO SHOWDOWN</Typography>
+          </div>
+          <Typography className="HomeBanner-description">
+            Text go here talking about it, bla bla bla, random cool text, uwu, it's cool here, i
+            like this text
+          </Typography>
+          {!mobileLayout && LoginButtons()}
+        </div>
+      </div>
+      <div className="HomeBanner-section-2">
+        <video autoPlay loop muted className="HomeBanner-media">
+          <source src="/public/banner.mp4" type="video/mp4" />
+        </video>
+        {mobileLayout && LoginButtons()}
+      </div>
     </div>
   );
 }
