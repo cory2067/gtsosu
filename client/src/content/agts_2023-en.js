7f9b1205374c9b9eb6c365486dd6232c6df24454
TsukoteAmie <thepeaceluffer@gmail.com>
2023-06-02 02:57:12 +0200

    delay

--- client/src/content/agts_2023-en.js
+++ client/src/content/agts_2023-en.js
@@ -24,21 +24,21 @@ export default {
   ### Registration Phase
   May 7th until May 21st.
   ### Screening and teams creation
-  May 21st until May 28th.
+  May 21st until June 4th.
   ### Qualifiers
-  June 3rd until June 4th.
-  ### Week 1/Group Stage
   June 10th until June 11th.
-  ### Week 2/Runoff bracket
+  ### Week 1/Group Stage
   June 17th until June 18th.
-  ### Week 3/Round Of 16 + Quarterfinals + Loser's Bracket Round 1
+  ### Week 2/Runoff bracket
   June 24th until June 25th.
-  ### Week 4/Semifinals + Loser's Bracket Round 2 and 3
+  ### Week 3/Round Of 16 + Quarterfinals + Loser's Bracket Round 1
   July 1st until July 2nd.
-  ### Week 5/Winner's Bracket Finals + Loser's Bracket Round 4 and 5
+  ### Week 4/Semifinals + Loser's Bracket Round 2 and 3
   July 8th until July 9th.
+  ### Week 5/Winner's Bracket Finals + Loser's Bracket Round 4 and 5
+  July 15th until July 16.
   ### Week 6/Grand Finals + Loser's Bracket Round 6 (Grand Finals) + All-Stars
-  July 15th until July 16.`,
+  July 22nd until July 23rd.`,
       },
       {
         title: "Prizes",
