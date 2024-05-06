f1841807dfbc6654ef920f536b3d73d212f0f2c7
TsukoteAmie <thepeaceluffer@gmail.com>
2024-05-06 21:47:56 +0200

    another year another btt

---
+++ client/src/content/btt_2024-en.js
@@ -0,0 +1,154 @@
+export default {
+  name: "Beginner's Taiko Tournament Season 9",
+  description:
+    "Beginner's Taiko Tournament is a rank-restricted tournament for newcomers to the taiko scene from infinity up to #10,000 rank. The players will first play in a qualifiers. The top 64 players will then play in a double-elimination bracket.",
+  links: [
+    {
+      label: "Discord",
+      link: "https://discord.gg/9FKYDe74un"
+    },
+    {
+      label: "Challonge",
+      link: "https://challonge.com/BTTS9"
+    },
+  ],
+  homepage: [
+    {
+      title: "Restrictions",
+      body: `1. You must be below #10000 in osu!taiko and you must stay on or below this line during the rest of the signup phase. A slight accidental trespass can be overlooked as long as you remain below #9900 AND you stop trying to gain pp the moment you pass #10,000.
+2. You are not allowed to rank up further than #9000 during the tournament (After registrations are closed). If you pass this limit at any moment, you will be disqualified. 
+3. You must join the Discord server of the tournament. 
+4. Though there will be no official screening, if the tournament staff determines that you are too skilled for this tournament, we reserve the right to remove you during the tournament at any moment.`,
+    },
+    {
+      title: "Registration",
+      body: `Please click on the Register button above after you've logged in with your osu! account, then click on the Discord button to join the server of our tournament.
+**Joining the Discord is mandatory to play in this tournament, and you will be removed from the tournament if you don't do so.**`,
+    },
+    {
+      title: "Dates",
+      body: `
+### Registration Phase
+May 5th until June 2nd.
+### Qualifiers
+June 8th until June 9th.
+### Round of 64
+June 15th until June 16th.
+### Round of 32 + Loser's Bracket Round 1
+June 22th until June 23th.
+### Round of 16 + Loser's Bracket Round 2 and 3
+June 29th until June 30th.
+### Quarterfinals + Loser's Bracket Round 4 and 5
+July 6th until July 7th.
+### Semifinals + Loser's Bracket Round 6 and 7
+July 13th until July 14th.
+### Winner's Bracket Finals + Loser's Bracket Round 8 and Finals
+July 20th until July 21st.
+### Grand Finals + Loser's Bracket Grand Finals + All-Stars
+July 27th until July 28th.`,
+    },
+    {
+      title: "Prizes",
+      body: `
+**1st place:** Special Discord role in the BTT server.
+There are no other prizes being considered for BTTS9
+`,
+    },
+  ],
+  rules: `
+# General Rules
+- This tournament is a **1v1 double-elimination tournament.**
+- The Score System used will be Score V2.
+- Any player who meets the requirements stated in the rank restrictions part is allowed to sign up.
+- Any members of staff apart from **Organizers, Mappoolers, Mappers and Referees** are allowed to play.
+- Players and staff members must have read this ruleset entirely (and we will assume you did so throughout the tournament).
+- All participants must stay respectful and keep a proper attitude. Not following this rule can result in a ban/blacklist from the tournament. This rule concerns all the staff as well.
+- Any rules changes or unexpected occurrences will be announced on the discord server.
+
+# Stage Regulations
+- There are eight stages to this tournament: Qualifiers, Round of 64, Round of 32, Round of 16, Quarterfinals, Semifinals, Finals and Grand Finals.
+- Participating players will be seeded based on the average of their ranks on each map during the Qualifiers round.
+- The winning conditions for each stage are as follows:
+  - Round of 64, Round of 32: Best-Of-7 (First to 4 points)
+  - Round of 16, Quarterfinals: Best-Of-9 (First to 5 points)
+  - Semifinals: Best-Of-11 (First to 6 points)
+  - Finals, Grand Finals: Best-Of-13 (First to 7 points)
+
+# Qualifiers Regulations
+- During this round there will be multiple lobbies available at various hours. 
+- A private match will be created and players will be invited into the lobby when it's their time. Make certain you're online at this time.
+- If a player who signed up to a lobby is late for that lobby, the lobby will be postponed up to 5 minutes before it starts.
+- Late players are allowed to join their lobby until 15 minutes after the lobby time (so they have 10 minutes after the lobby starts). They join the others wherever they are in the quals pool and play the maps they missed after everyone else is done.
+- Players who still haven't shown up will need to reschedule to a later time.
+- There are no warmups.
+- Failed scores will count. NoFail will be enforced.
+- Seedings will be done with the average rank of each player on each map. 
+- Maps will be played once in a row during the lobby. 
+- Only the 64 best players will continue on to the bracket stage.
+- Qualifier scores are to remain private as much as possible until the results are released by the staff. So don't post or stream your qualifiers!
+
+# Mappool Instructions
+- There will be 1 mappool for each of the following: Qualifiers, Round of 64, Round of 32, Round of 16, Quarterfinals, Semifinals, Finals and Grand Finals.
+- The Loser's Bracket will play on the same pool as the Winner's bracket of the same weekend.
+- The mappools will consist of the following brackets: NoMod, Hidden, Hardrock, DoubleTime, HiddenHardrock and TieBreaker.
+- The qualifiers pool will be different from all the other rounds, as it will have 3 NoMod maps, 2 Hidden maps, 2 HardRock maps, and 1 DoubleTime map.
+- The NoMod bracket will have 4 maps in Round of 64 till Quarterfinals, 5 maps in Semifinals, and 6 maps in Finals and Grand Finals
+- The Hidden and Hardrock brackets will have 2 maps each in Round of 64 and Round of 32, and 3 maps each in all following rounds.
+- The DoubleTime bracket will have 2 maps in Round of 64 till Round of 16, and 3 maps in all following rounds.
+- The HiddenHardrock bracket will have 1 map in both the Finals and Grand Finals. There will be none in earlier rounds.
+- The TiebBeaker is played with FreeMod, meaning it can be played with **NoMod, Hidden, HardRock, or Hidden + Hardrock**. You do not have to play with a mod, but it does give a significant score boost if you wish to try and practice it.
+- The mappool for each stage will be showcased a week beforehand, on sunday.
+
+# Scheduling Instructions
+- Every stage is held **during the weekend**. (It is however allowed to schedule on another day if no agreement is found on a time during the weekend) 
+- Matches are allowed to overlap if referees are available. 
+- We will try to schedule as best as possible so it fits with the time zones. The schedule will be released the weekend before each stage. 
+- Reschedules will only be considered if both players agree on a time. Proof of that (screenshot) along with the new match time needs to be sent in #reschedules before **Thursday at 23:59 UTC+0** in that particular week when your match takes place.
+- If a player is not responding to a reschedule request, the hosts (TaikoMom or Astraea) should be made aware of this so they can look into this on time.
+- If a player is not cooperating on a reschedule and there are no given reasons as to why, **the host may decide to make it a wbd for the other player**. This is done on a case-by-case basis and can only be decided by a host.
+- If there is **hard evidence** of a player denying a reschedule request (screenshots and such), **a host may immediately kick the player from the tournament and permanently ban them from ever participating again**. This is, again, done on a case-by-case basis and can only be decided by a host.
+
+# Match Regulations
+- A private match will be created 10-15 minutes before your match by a referee. Both players will be invited when it's match time. Make certain you're online at this time.
+- The match will be played with HeadToHead and ScoreV2. The name of the lobby must be: "BTTS9: (Red Player) vs (Blue Player)
+- If no referee is available, the match will be postponed.
+- If one of the players is late, the maximum time the match can be postponed is 15 minutes. If there is still just 1 player in the lobby after the 15-minute time limit, the present player will win by default (WBD).
+- There is no penalty for not joining the room instantly after match time as long as it's within the postponed time.
+- If neither of the players are in the lobby after 15 minutes, the highest seeded player will advance.
+- Each player is allowed one warm-up. It must be below 3:00 in drain time. **No exceptions.**
+- After both warm-ups have been played, the referee will ask the players to roll. This is done by typing '!roll' in the chat. The player with the highest number wins the roll.
+- Each player has to ban **one beatmaps**. These maps cannot be picked by either player for the entire match.
+- The roll loser bans first. The roll winner bans second.
+- Players have 2 minutes to announce their ban. If they did not announce their ban in time, their ban will be made with a roll by the referee.
+- The TieBreaker cannot be banned. A player and their opponent can both ban a map from the same modpool. If a player bans a DT, their opponent can ban another DT if they want.
+- After the bans, the roll winner will pick the first map to be played. After the map, the roll loser will pick the second map. Picks will then alternate between the players.
+- Like with the bans, players have 2 minutes to announce their pick to the referee. If they did not announce their pick in time, their pick will be made with a roll by the referee.
+- Any map that was **NOT** banned, already played, or the TieBreaker can be picked.
+- Players will have 2 minutes to pick a beatmap and 2 minutes to get ready. If a player didn't tell their pick before the end of the timer the map will be picked with a roll. And if players do not get ready before the end of the timer, the match will be started, no matter what.
+- Players can at any time ask for a timeout of max 10 minutes. Each player can do so only once per match.
+- If a player disconnects between the beatmaps, the match can be delayed 10 minutes at maximum. This delay can only be applied once per player per match, but is counted separately from a timeout.
+- Disconnects within 30 seconds after beatmap begin can be rematched. This is only allowed once per player.
+- If a player disconnects after 30 seconds, they get treated as if they lost the map.
+- If a player disconnects after 30 seconds, but can show a screenshot the local score with a **fitting time stamp**, and it's higher than the opponent's score, they still win the point. It is up to the referee to decide if its legit.
+- If someone tries to abuse the rule above and we see you're trying to pass a different score, you will instantly lose the match.
+- In case NoFail is forgotten by players during the tiebreaker, failed scores count.
+- If the beatmap ends in a draw, the result will be nullified and the map will be replayed. If it ends in another tie, both the result and the map will be voided and the player who picked it will have to pick another map. The 
+- If both players reach match point (1 more point needed to win), the TieBreaker will be played.
+- Results will be displayed on the website, on Challonge, and announced by the referee in the #match-feed channel of the Discord server.
+- If both players tie on the TieBreaker, the TieBreaker will be replayed until there is no longer a tied result. The TieBreaker cannot be voided.
+
+# All-Star Rules
+- All-Star is a fun event taking place on the same weekend as the Grand Finals! 
+It'll showcase the best players of the tournament playing against each other in a special match. 
+- 16 people will participate in this event, 8 players and 8 staff members. They will be decided by vote. 
+- The top 16 will play in All-Star.
+- Teams are made with equal number of players and staff, meaning both sides will have 4 players and 4 staff members.
+- The top 2 people will be captains of each team. Their teammates will be decided by the ranking of the next 10 people. The topmost voted player will play with players with an odd number and the second most voted player with players with an even number. 
+- The event will be a 4 vs 4 match with the following rules: 
+  - BO15. 
+  - Mappool will be the Grand Finals mappool.
+  - No bans. 
+  - Map selection is the same as a normal match. 
+  - Same rules as a normal match for disconnections. 
+`,
+};
