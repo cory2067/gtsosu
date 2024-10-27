83618c985aec270072c3c9e6e77cf44bbd5b9563
Kasumiikun <13593112+Kasumisama@users.noreply.github.com>
2024-10-27 22:19:18 +0100

    update merch link

--- client/src/content/ui.js
+++ client/src/content/ui.js
@@ -10,7 +10,7 @@ export default {
     merch: {
       title: "Merch",
       mainStore: "Main Store",
-      mousepads: "Mousepads",
+      mousepads: "Mousepads (now down)",
     },
     login: "Log In",
     logout: "Log Out",
