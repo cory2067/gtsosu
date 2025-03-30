d5699f82566f61690669bbb655e5a94d28649421
TsukoteAmie <thepeaceluffer@gmail.com>
2025-03-30 22:11:35 +0200

    2025 time bby

---
+++ client/src/content/rgts_2025-en.js
@@ -0,0 +1,214 @@
+export default {
+    name: "Rising Global Taiko Showdown",
+    description:
+      "The Rising Global Taiko Showdown. It is our 2v2 tournament for mid-level players being restricted for ranks #1,500 to #4,500. The teams will first play in a qualifiers. Then they will play in a Group Stage, a Runoff Bracket and finally in a Double-elimination Bracket.",
+    links: [
+      {
+        label: "Discord",
+        link: "https://discord.gg/3mGC3HB"
+      },
+      {
+        label: "Challonge Week 1: Group Stage",
+        link: ""
+      },
+      {
+        label: "Challonge Week 2: Runoff Bracket",
+        link: ""
+      },
+      {
+        label: "Challonge Week 3-6: Main Bracket",
+        link: ""
+      },
+      {
+        label: "Pick'ems",
+        link: ""
+      },
+    ],
+    homepage: [
+      {
+        title: "Restrictions",
+        body: `1. You must be between ranks #1,500 - #4,500 in osu!taiko during the entirety of the registration phase.
+  2. You are not allowed to rank up further than #400 during the tournament. If you pass this limit you'll be disqualified. 
+  3. You must join the Discord server of the tournament.
+  4. You must not have been part of a team that has placed top 3 in the past 2 iterations or that has ever won RGTS.
+  5. You must not have been part of a team that has placed top 3 in any of the higher rank-restricted GTS tournament (AGTS).
+  6. Any osu!taiko player who follows these criterias will be allowed in this tournament. The GTS team reserves the right of removing any player from the tournament during the registration and teams creation phase.`,
+      },
+      { 
+        title: "Registration", 
+        body: `**Please click on the Register button above after you've logged in with your osu! account, then click on the Discord button to join the server of our tournament.**
+  
+  **Joining the Discord is mandatory to play in this tournament, and you will be removed from the tournament if you don't do so.**`,
+      }, 
+      {
+        title: "Dates",
+        body: `
+  ### Registration Phase
+  March 30th until April 6th.
+  ### Teams creation
+  April 6th until April 10th.
+  ### Qualifiers
+  April 12th until April 13th.
+  ### Week 1/Group Stage
+  April 19th until April 20th.
+  ### Week 2/Runoff bracket
+  April 26th until April 27th.
+  ### Week 3/Quarterfinals + Loser's Bracket Round 1
+  May 3rd until May 4th.
+  ### Week 4/Semifinals + Loser's Bracket Round 2 and 3
+  May 10th until May 11th.
+  ### Week 5/Winner's Bracket Finals + Loser's Bracket Round 4 and 5
+  May 17th until May 18th.
+  ### Week 6/Grand Finals + Loser's Bracket Round 6 (Grand Finals) + All-Stars
+  May 24th until May 25th.`,
+      },
+      {
+        title: "Prizes",
+        body: `
+  - **1st place:** First place team banner
+  - **2nd place:** Second place team banner
+  - **3rd place:** Third place team banner`,
+      },
+    ],
+    rules: `
+  # General Rules
+  - This tournament is a **2v2 team tournament** with teams of **5 players maximum**.
+  - The teams will first go through **Qualifiers**. Then they will play in a **Group Stage**, a **Runoff Bracket** and finally in a **Double-elimination Bracket**.
+  - The Score System used will be **Score V2**.
+  - Any player who meets the requirements stated in the rank restrictions part is allowed to sign up. Since this tournament will not be badged, some players may be declined by GTS staff if they are deemed too skilled for the tournament despite being in the rank range.
+  - Any member of **staff** (**Organizers, Mappoolers, Playtesters/Replayers, and Custom Mappers**) is not allowed to play. Other positions considered as **helpers** (**Referees, Commentators, and Streamers**) are allowed to play.
+  - Players who have been eliminated from the tournament are allowed to join staff as **Playtesters/Replayers**. Other staff roles are not allowed. They can also join the **helpers** at any moment.
+  - Players and staff members must have read this ruleset entirely (and we will assume you did so throughout the tournament).
+  - All participants must stay respectful and keep a proper attitude. Not following this rule can result in a ban/blacklist from the tournament. This rule concerns all the staff as well.
+  - Any rule changes or unexpected occurrences will be announced in the Discord server.
+  
+  # Tournament Registration and Teams Creation
+  - In order to participate you need to meet the criteria stated in the **rank restrictions** part.
+  - You are not allowed to rank up further than #500 during the registration period and then further than #400 during the actual tournament. If you pass this limit you'll be disqualified.
+  - GTS staff can refuse a player from participating in the tournament if they deem his skill is much higher than the targeted rank range.
+  - Players sign up individually. When their registration is accepted, they will get a country role.
+  - A country needs **at least 2 players** to participate.
+  - Players from countries or territories (as seen on their profile flag) that are under a protectorate, sovereign state, or otherwise closely related to another country may choose to play for that country if the country listed on their profile is unable to field enough players to form a valid team.
+  - If there are 35 or more unique countries able to participate after screening, only 1 team per country will be allowed. In that case, if there are more than 5 players signed up for a country they will decide among themselves who will partake in the tournament. If they cannot decide the top 5 players by ranking will be chosen to form the team.
+  - If there are less than 35 unique countries, we are **allowing 2 teams** for a country under certain conditions:
+    - There are **7 or more players** signed up for a country.
+    - Each team has to have **at least 3 players**.
+  - Because of the previous rule there are going to be different situations when creating the teams:
+    - If there are 5 players or less signed up for a country, those players will form the team representing their country.
+    - In case there are more than 10 players signed up for a country, players will decide among themselves who will partake in the tournament. If they cannot decide the top 10 players by ranking will be chosen to form the 2 teams.
+    - If there are 6 players, players will have to decide on the 5 players who will partake in the tournament. If they cannot decide the top 5 players by ranking will be chosen to form the team.
+    - If there are 7 to 10 players (after keeping the best ranked ones) signed up for a country, the players from that country will have a few days to decide how to split themselves between the 2 teams.
+    - In case the players can't come to an agreement, they will be ranked by pp and put in the 2 teams with at least 3 members in each of them with the highest ones in a first team and the lowest in the second.
+  - It is recommended to come to an agreement as fast as possible to then decide on the captain and be able to schedule and prepare your matches rapidly.
+    - The players will decide on the captain of their team.
+    - If the players can't agree or find a captain, the highest ranked player will be chosen.
+    - The captain is going to be the one to reschedule a match if needed and will ensure that his teammates are here for the matches.
+  
+  # Stage Regulations
+  ![](https://motheroftaiko.s-ul.eu/oIDm9ntV)
+  - There are 4 main stages in this tournament: the Qualifiers, the Group Stage, the Runoff Bracket, the Main Bracket.
+  - Teams will be seeded with the average of their ranks on each map during the Qualifiers round in 4 seeds: Top seed, High seed, Low seed and Unseeded. (The seeding process is explained more precisely in the Qualifers Instructions part)
+  - Teams seeded 1-16 skip the first week.
+  - **Group Stage**
+    - Teams seeded 17-32 will be drawn in **4 groups of 4 teams** during a drawing show which will happen on Sunday April 13th.
+    - In the Group Stage all the teams from each group will face each other. The **best 2 teams** get out of their group.
+    - Rankings of each group are determined by sorting the results of each team's performance in the following priority:
+        - Most matches won.
+        - Highest (number of beatmaps won - number of maps lost) score.
+        - Most beatmaps won.
+        - Winner of the match played previously between the tied teams.
+    - In Group stage, 'Win by default' will be considered as win by 5:0, +1.0 score difference ratio.
+    - The top 2 teams of each group will join the Runoff Bracket in Tier 3.
+  - **Runoff Bracket**
+    - The Runoff Bracket is an intermediate bracket consisting of 3 Tiers in which teams will compete to have a chance to play in the main bracket. Tier 1 has 1 round of matches, Tier 2 and 3 have 2 rounds of matches.
+      - Tier 1 will see the teams seeded 1-8 play against each other (1 will play against 8 and so on).
+        - The winners of these matches will go on to play in the main bracket. The losers will play against the winners of the 1st round of Tier 2.
+      - Tier 2 will see the teams seeded 9-16 play against each other (9 will play against 16 and so on).
+        - The winners of these matches will play against the losers of Tier 1.
+        - The losers will play against the winners of the Tier 3 round 1 matches.
+      - Tier 3 will see teams who finished first place in their group play against a second place team (1st of A vs 2nd of D, 1st of B vs 2nd of C, 1st of C vs 2nd of B, 1st of D vs 2nd of A)
+        - The winners of these matches will go on to play against the losers of Tier 2 round 1. The losers are eliminated.
+      - The 2nd round of the Runoff Bracket matches will keep the order from the first set of matches
+        - For Tier 2: Loser of 1 vs 8 plays against winner of 9 vs 16, etc.
+        - For Tier 3: Loser of 9 vs 16 plays against winner of 1st of A vs 2nd of D, etc.
+    - The winners of Tier 1 and Tier 2 round 2 will start in the Winners Bracket of the main bracket. The losers of Tier 2 round 2 and the winners of Tier 3 round 2 will start in the Losers Bracket of the Main bracket. The losers of Tier 3 round 2 are eliminated from the tournament.
+  - **Main Bracket**
+    - In this stage, the tournament will follow a classic double-elimination bracket format.
+    - The Winners Bracket will consist of the winners of the Tier 1 Winners Bracket round facing the winners of the Tier 2 2nd Losers Bracket Round.
+    - The Losers Bracket will consist of the losers of the Tier 2 2nd Losers Bracket Round facing the winners of the Tier 3 2nd Losers Bracket Round
+  - The winning condition for each stage is:
+    - **Week 1/Group Stage: BO9 (win 5 maps)**
+    - **Week 2 & 3/Runoff bracket, Quarterfinals + Loser's Bracket Round 1: BO11 (win 6 maps)**
+    - **Week 4 & 5 & 6/Semi-Finals, Finals, Grand Finals + Loser's Bracket Round 2, 3, 4, 5 and 6: BO13 (win 7 maps)**
+  
+  # Qualifiers Regulations
+  - During this round there will be multiple lobbies available at various hours.
+  - A private match will be created and teams will be invited into the lobby when it's their time. Make certain you're online at this time.
+  - There are no warmups.
+  - Failed scores will count.
+  - Only two players from a team will be playing per map. There is no limitations on player swapping.
+  - Seedings will be done with the average rank of each team on each map.
+  - If two teams are tied, the team who has the highest combined score of their best runs across all maps, will be seeded higher.
+  - Maps will be played twice in a row during the lobby. The best run will be taken into account for the ranking.
+  - Only the 32 best teams will continue on.
+  
+  # Match Regulations
+  - A private lobby will be created 10 minutes before your match by a referee. Both team's captains will be invited into the room when it's match time. Then the referee or the captains will invite the other players into the room.
+  - If no staff or referee is available the match will be postponed.
+  - If less than the minimum required players attend, the maximum time the match can be postponed is 10 minutes.
+  - There is no penalty for not joining the room instantly after match time as long as it's within the postponed time.
+  - If neither of both teams have the minimum required players after 10 minutes it will count as a loss for both teams in Group Stage. In the Runoff Bracket or the Main Bracket the highest seeded team will advance.
+  - The match will be played as TeamVs and ScoreV2 and the name of the room must be: "RGTS2025: (Team Red) vs (Team Blue)".
+  - There will be no warmups.
+  - Each captain has to ban **one beatmap** from the pool (apart from Tiebreaker). These beatmaps are not allowed to be picked by any team in the entire match.
+  - The captain of each team will roll with the command "!roll" in the chat.
+  - The team with the highest roll **bans second and picks first**. The team with the lowest roll **bans first and picks second**.
+  - Each captain has 2 mins to announce their ban. If they didn't tell the referee by that time the ban will be made with a roll.
+  - Each team is allowed a timeout of 2 mins per match. This timeout is optional.
+  - Beatmap selection will then alternate between each captain selecting a beatmap out of the map pool.
+  - Captains may pick freely from any bracket.
+  - Teams will have 2 minutes to pick a beatmap and 2 minutes to get ready. If a team didn't tell their pick before the end of the timer the map will be picked with a roll.
+  - Exchanging players during a match is allowed without limitations.
+  - If a player disconnects between the beatmaps and the team cannot provide an exchange the match can be delayed 10 minutes at most. This delay can only be applied once.
+  - If a player disconnects, they get treated as if they failed the map.
+  - Disconnects within 30 seconds after the beatmap was started allows the map to be replayed. This is only allowed once per team.
+  - Failed players' scores do get added to the team score.
+  - If the beatmap ends in a draw the game will be nullified and the map will be replayed.
+  - In case of a tie in points (example: 4-4 in BO9) the tiebreaker is selected.
+  
+  # Mappool Instructions
+  - There will be 1 mappool for each of the following stage: Qualifiers, Week 1, Week 2, Week 3, Week 4, Week 5 and Week 6.
+  - The Qualifiers pool will be different from all the other rounds as it will have a format of 3 NoMod maps, 1 Hidden map, 1 HardRock map, 1 DoubleTime map and 1 FreeMod map.
+  - The Loser's Bracket will play on the same pool as the Winner's bracket of the same weekend.
+  - Each mappool consists of 6 brackets: NoMod, Hidden, HardRock, DoubleTime, FreeMod and Tiebreaker.
+  - The Week 1 mappool will consist of 13 maps. Week 2 and Week 3 mappool will consist of 14 maps. Week 4, Week 5 and Week 6 mappool will consist of 15 maps.
+  - The size of the Hidden, HardRock and DoubleTime brackets will be 2 in all stages.
+  - The size of the NoMod bracket will be 4 in Week 1, Week 2 and Week 3; 5 in Week 4, Week 5 and Week 6.
+  - The size of the FreeMod bracked will be 2 in Week 1; 3 for the rest of the tournament.
+  - The FreeMod pool is played with FreeMod activated. Every individual player can pick **Hidden, HardRock or both**. **At least one player of each team must have at least one mod activated**.
+  - Players must select **different mod combinations** per team. **Both players using Hidden, HardRock or Hidden+HardRock is disallowed**.
+  - The Tiebreaker is played under FreeMod conditions but it is not necessary that at least one player has at least one mode activated.
+  - The mappool for each stage will be showcased a week beforehand.
+  
+  # Scheduling Instructions
+  - Every stage is held **during the weekend**. (It is however allowed to schedule on another day if no agreement is found on a time during the weekend)
+  - Matches are allowed to overlap if referees are available.
+  - We will try to schedule as best as possible so it fits with the time zones. The schedule will be released the previous weekend of each stage.
+  - Reschedules will only be considered if both teams agree on a time. Proof of that needs to be sent to the tournament staff before **Thursday at 23:59 UTC+0** in that particular week when your match takes place.
+  - Reschedules may only be requested by **a team captain**.
+  
+  # All-Star Rules
+  - All-Star is a fun event taking place on the same weekend as the Grand Finals!
+  It'll showcase the best players of the tournament playing against each other in a special match.
+  - 8 players will participate in this event. They will be decided by vote.
+  - The top 8 players will play in All-Star. If there are more than 2 players from a country in the top 8 most voted players only the 2 best will partake in All-Star.
+  - The top 2 players will be captains of each team. The rest of the teams will be decided by the hosts.
+  - The Grand Finals mappool will be used.
+  - The event will be a 4 vs 4 match with the following rules:
+    - BO15.
+    - No bans.
+    - Map selection is done via a !roll command from the referee.
+    - Same rules as a normal match for disconnections.
+    `,
+  };
+  
