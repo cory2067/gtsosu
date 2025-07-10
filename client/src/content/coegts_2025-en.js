ea0a8da5714229851f13a2e4bf96be71b8e96701
Kasumi <13593112+Kasumisama@users.noreply.github.com>
2025-07-10 22:31:17 +0200

    update coegts rules

--- client/src/content/coegts_2025-en.js
+++ client/src/content/coegts_2025-en.js
@@ -1,7 +1,7 @@
 export default {
   name: "cavoe's osu! event's Global Taiko Showdown",
   description:
-    "The cavoe's osu! event's Global Taiko Showdown, our first ever offline 1v1 tournament, with entirely custom songs. The top 8 players after online qualifiers will then face off in a single-elimination bracket, live at COE's stage.",
+    "The cavoe's osu! event's Global Taiko Showdown, our first ever offline 1v1 tournament, with entirely custom songs. The top 16 players after online qualifiers will then face off in a single-elimination bracket, live at COE's stage.",
   links: [
     {
       label: "Discord",
@@ -24,13 +24,13 @@ July 20th.
 July 21st - July 27th.
 ### Qualifier Results + Main Tournament Mappool Showcase (On COE's stage, streamed on Twitch as well.)
 July 28th, 19:00 (UTC+2).
-### Main Tournament Day 2 (Round of 16 pt.1)
+### Main Tournament Day 1 (Round of 16 pt.1)
 July 30th, 16:00 to 21:00 (UTC+2, 21:00 being the end of the last match.)
-### Main Tournament Day 1 (Round of 16, pt.2 and Quarterfinals)
+### Main Tournament Day 2 (Round of 16, pt.2 and Quarterfinals)
 July 31st, 11:00 to 16:00 for Round of 16, 17:00 to 22:00 for Quarterfinals (UTC+2, 22:00 being the end of the last match.)
-### Main Tournament Day 2 (Semifinals)
+### Main Tournament Day 3 (Semifinals)
 August 1st, 19:30 to 22:30 (UTC+2, 22:30 being the end of the last match.)
-### Main Tournament Day 3 (Match for 3rd place & Grand Finals)
+### Main Tournament Day 4 (Match for 3rd place & Grand Finals)
 August 2nd, 10:30 to 12:00 for the Match for 3rd place, & 15:30-17:30 for Grand Finals. (UTC+2, 17:30 being the end of the last match.)`,
 
     },
