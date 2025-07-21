bf32a91f3da57ed2c4c2fd4be4580d07c2c78efe
Kasumiikun <13593112+Kasumisama@users.noreply.github.com>
2025-07-21 17:03:52 +0200

    ygts again

---
+++ client/src/content/ygts_2026-en.js
@@ -0,0 +1,36 @@
+export default {
+  name: "Random Global Taiko Showdown",
+  description:
+    "no seriously why",
+  links: [
+    {
+      label: "Discord",
+      link: "https://discord.gg/3mGC3HB"
+    },
+    {
+      label: "Challonge",
+      link: ""
+    },
+  ],
+  homepage: [
+    {
+      title: "Restrictions",
+      body: `no seriously why`,
+    },
+    {
+      title: "Dates",
+      body: `
+today`
+
+    },
+    { title: "Registration", body: "Please click on the Register button above after you've logged in with your osu! account, then click on the Discord button to join the server of our tournament. **Joining the Discord is mandatory to play in this tournament, and you will be removed from the tournament if you don't do so.**" }, 
+    {
+      title: "Prizes",
+      body: `
+no`,
+    },
+  ],
+  rules: `
+  random
+   `,
+};
